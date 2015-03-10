305c030be0b219b3420b3937cfa9ac258ef27a08
Mike Burke <mike@livioconnect.com>
2015-03-10 16:52:32 -0400

    Fixes #142, fixes #143, fixes #144, fixes #145
    Improvements to LockScreenManager
    
    Signed-off-by: Mike Burke <mike@livioconnect.com>

--- sdl_android_lib/src/com/smartdevicelink/proxy/LockScreenManager.java
+++ sdl_android_lib/src/com/smartdevicelink/proxy/LockScreenManager.java
@@ -4,90 +4,51 @@ import com.smartdevicelink.proxy.rpc.OnLockScreenStatus;
 import com.smartdevicelink.proxy.rpc.enums.HMILevel;
 import com.smartdevicelink.proxy.rpc.enums.LockScreenStatus;
 
-public class LockScreenManager {
-	
-	private Boolean bUserSelected = false;
-	private Boolean bDriverDistStatus = null;
-	private HMILevel  hmiLevel = null;
-	@SuppressWarnings("unused")
-    private int iSessionID;
+public class LockScreenManager{
 
-	public synchronized void setSessionID(int iVal)
-	{
-			iSessionID = iVal;
-	}
-	
-	private synchronized void setUserSelectedStatus(boolean bVal)
-	{
-			bUserSelected = bVal;
-	}	
+    private Boolean  bDriverDistStatus = null;
+    private HMILevel hmiLevel          = null;
 
-	public synchronized void setDriverDistStatus(boolean bVal)
-	{
-			bDriverDistStatus = bVal;	
-	}
+    @Deprecated
+    public synchronized void setSessionID(int iVal){/* do nothing */}
 
-	public synchronized void setHMILevel(HMILevel hmiVal)
-	{		
-		hmiLevel = hmiVal;
-		
-		if ( (hmiVal.equals(HMILevel.HMI_FULL)) || (hmiVal.equals(HMILevel.HMI_LIMITED)) )
-			setUserSelectedStatus(true);
-		else if (hmiVal.equals(HMILevel.HMI_NONE))
-			setUserSelectedStatus(false);				
-	}
-	
-	public synchronized OnLockScreenStatus getLockObj(/*int SessionID*/)
-	{
-		//int iSessionID = SessionID;
-		OnLockScreenStatus myLock = new OnLockScreenStatus();
-		myLock.setDriverDistractionStatus(bDriverDistStatus);
-		myLock.setHMILevel(hmiLevel);
-		myLock.setUserSelected(bUserSelected);
-		myLock.setShowLockScreen(getLockScreenStatus());
-		
-		return myLock;
-	}
-	
-	private synchronized LockScreenStatus getLockScreenStatus() 
-	{
-		
-		if ( (hmiLevel == null) || (hmiLevel.equals(HMILevel.HMI_NONE)) )
-		{
-			return LockScreenStatus.OFF;
-		}
-		else if ( hmiLevel.equals(HMILevel.HMI_BACKGROUND) )
-		{
-			if (bDriverDistStatus == null)
-			{
-				//we don't have driver distraction, lockscreen is entirely based on userselection
-				if (bUserSelected)
-					return LockScreenStatus.REQUIRED;
-				else
-					return LockScreenStatus.OFF;
-			}
-			else if (bDriverDistStatus && bUserSelected)
-			{
-				return LockScreenStatus.REQUIRED;
-			}
-			else if (!bDriverDistStatus && bUserSelected)
-			{
-				return LockScreenStatus.OPTIONAL;
-			}
-			else
-			{
-				return LockScreenStatus.OFF;
-			}
-		}
-		else if ( (hmiLevel.equals(HMILevel.HMI_FULL)) || (hmiLevel.equals(HMILevel.HMI_LIMITED)) )
-		{
-			if ( (bDriverDistStatus != null) && (!bDriverDistStatus) )
-			{
-				return LockScreenStatus.OPTIONAL;
-			}
-			else
-				return LockScreenStatus.REQUIRED;
-		}
-		return LockScreenStatus.OFF;
-	}
+    public synchronized void setDriverDistStatus(Boolean bVal){
+        bDriverDistStatus = bVal;
+    }
+
+    public synchronized void setHMILevel(HMILevel hmiVal){
+        hmiLevel = hmiVal;
+    }
+
+    public synchronized OnLockScreenStatus getLockObj(){
+        OnLockScreenStatus myLock = new OnLockScreenStatus();
+        myLock.setDriverDistractionStatus(bDriverDistStatus);
+        myLock.setHMILevel(hmiLevel);
+        myLock.setUserSelected(calculateUserSelected(hmiLevel));
+        myLock.setShowLockScreen(getLockScreenStatus());
+
+        return myLock;
+    }
+
+    private synchronized LockScreenStatus getLockScreenStatus(){
+        LockScreenStatus result = LockScreenStatus.OFF;
+        HMILevel temp = ( hmiLevel == null ) ? HMILevel.HMI_NONE : hmiLevel;
+
+        if(calculateUserSelected(temp)){
+            result = driverDistStatus();
+        }
+
+        return result;
+    }
+
+    private synchronized LockScreenStatus driverDistStatus(){
+        if(bDriverDistStatus == null || bDriverDistStatus){
+            return LockScreenStatus.REQUIRED;
+        }
+        return LockScreenStatus.OPTIONAL;
+    }
+
+    private static boolean calculateUserSelected(HMILevel hmiLevel){
+        return ( hmiLevel != null && ( hmiLevel == HMILevel.HMI_FULL || hmiLevel == HMILevel.HMI_LIMITED ) );
+    }
 }
