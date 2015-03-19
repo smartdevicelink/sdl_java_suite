4e23616c92071c562327d6db18ff194d7151e42c
Joey Grover <joeygrover@gmail.com>
2015-03-19 10:56:26 -0400

    Adding basic SDL project so developers can copy and paste some code

---
+++ HelloSdl/src/com/hellosdl/sdl/SdlReceiver.java
@@ -0,0 +1,35 @@
+package com.hellosdl.sdl;
+
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+
+public class SdlReceiver extends com.smartdevicelink.transport.SdlBroadcastReceiver {
+	private static final String TAG = "BCast Receiver";
+
+	@Override
+	public Class defineLocalSdlRouterClass() {
+		return SdlRouterService.class;
+	}
+
+	@Override
+	public void onSdlEnabled(Context context) {
+		Log.e(TAG, "SDL Enabled");
+		Intent startIntent = new Intent(context, SdlService.class);
+		startIntent.putExtra(FORCE_TRANSPORT_CONNECTED, true);
+		context.startService(startIntent);
+	}
+
+	@Override
+	public void onSdlDisabled(Context context) {
+		Intent startIntent = new Intent(context, SdlService.class);
+		context.stopService(startIntent);
+		
+	}
+	
+
+
+}
