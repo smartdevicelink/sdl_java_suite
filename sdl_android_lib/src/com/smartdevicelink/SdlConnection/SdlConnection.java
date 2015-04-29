89419856b09ca7a4c95bad7417377b0451b56dab
Markos Rapitis <mrapitis@ford.com>
2015-04-29 09:29:18 -0400

    Made changes to remove SESSION_LOCK and replace contains method

--- sdl_android_lib/src/com/smartdevicelink/SdlConnection/SdlConnection.java
+++ sdl_android_lib/src/com/smartdevicelink/SdlConnection/SdlConnection.java
@@ -34,7 +34,6 @@ public class SdlConnection implements IProtocolListener, ITransportListener, ISt
 	Object TRANSPORT_REFERENCE_LOCK = new Object();
 	Object PROTOCOL_REFERENCE_LOCK = new Object();
 	
-	private Object SESSION_LOCK = new Object();
 	private CopyOnWriteArrayList<SdlSession> listenerList = new CopyOnWriteArrayList<SdlSession>();
 	
 	/**
@@ -397,11 +396,7 @@ public class SdlConnection implements IProtocolListener, ITransportListener, ISt
 		}
 	}
 	void registerSession(SdlSession registerListener) throws SdlException {
-		synchronized (SESSION_LOCK) {
-			if (!listenerList.contains(registerListener)) {
-				listenerList.add(registerListener); //TODO: check if we need to sort the list.
-			}
-		}
+		listenerList.addIfAbsent(registerListener);
 		
 		if (!this.getIsConnected()) {
 			this.startTransport();
@@ -416,12 +411,8 @@ public class SdlConnection implements IProtocolListener, ITransportListener, ISt
 	}	
 	
 	public void unregisterSession(SdlSession registerListener) {
-		synchronized (SESSION_LOCK) {
-			listenerList.remove(registerListener);
-		
-		
+		listenerList.remove(registerListener);			
 		closeConnection(listenerList.size() == 0, registerListener.getSessionId());
-		}
 	}
 
 	
