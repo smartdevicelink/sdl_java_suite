240226b78c8625397d1b8fea02f1c0804b68db5b
Ty Strayer <tstrayer@ford.com>
2015-09-30 18:22:01 -0400

    Stubbed in methods in SdlManager that the bluetooth connection will need to start and stop the proxy. These are package protected.

--- sdl_android_lib/src/com/smartdevicelink/lifecycle/SdlManager.java
+++ sdl_android_lib/src/com/smartdevicelink/lifecycle/SdlManager.java
@@ -1,12 +1,419 @@
 package com.smartdevicelink.lifecycle;
 
-public class SdlManager {
+import android.util.SparseArray;
+
+import com.smartdevicelink.exception.SdlException;
+import com.smartdevicelink.protocol.enums.FunctionID;
+import com.smartdevicelink.proxy.RPCRequest;
+import com.smartdevicelink.proxy.SdlProxyALM;
+import com.smartdevicelink.proxy.callbacks.OnServiceEnded;
+import com.smartdevicelink.proxy.callbacks.OnServiceNACKed;
+import com.smartdevicelink.proxy.interfaces.IProxyListenerALM;
+import com.smartdevicelink.proxy.rpc.AddCommandResponse;
+import com.smartdevicelink.proxy.rpc.AddSubMenuResponse;
+import com.smartdevicelink.proxy.rpc.AlertManeuverResponse;
+import com.smartdevicelink.proxy.rpc.AlertResponse;
+import com.smartdevicelink.proxy.rpc.ChangeRegistrationResponse;
+import com.smartdevicelink.proxy.rpc.CreateInteractionChoiceSetResponse;
+import com.smartdevicelink.proxy.rpc.DeleteCommandResponse;
+import com.smartdevicelink.proxy.rpc.DeleteFileResponse;
+import com.smartdevicelink.proxy.rpc.DeleteInteractionChoiceSetResponse;
+import com.smartdevicelink.proxy.rpc.DeleteSubMenuResponse;
+import com.smartdevicelink.proxy.rpc.DiagnosticMessageResponse;
+import com.smartdevicelink.proxy.rpc.DialNumberResponse;
+import com.smartdevicelink.proxy.rpc.EndAudioPassThruResponse;
+import com.smartdevicelink.proxy.rpc.GenericResponse;
+import com.smartdevicelink.proxy.rpc.GetDTCsResponse;
+import com.smartdevicelink.proxy.rpc.GetVehicleDataResponse;
+import com.smartdevicelink.proxy.rpc.ListFilesResponse;
+import com.smartdevicelink.proxy.rpc.OnAudioPassThru;
+import com.smartdevicelink.proxy.rpc.OnButtonEvent;
+import com.smartdevicelink.proxy.rpc.OnButtonPress;
+import com.smartdevicelink.proxy.rpc.OnCommand;
+import com.smartdevicelink.proxy.rpc.OnDriverDistraction;
+import com.smartdevicelink.proxy.rpc.OnHMIStatus;
+import com.smartdevicelink.proxy.rpc.OnHashChange;
+import com.smartdevicelink.proxy.rpc.OnKeyboardInput;
+import com.smartdevicelink.proxy.rpc.OnLanguageChange;
+import com.smartdevicelink.proxy.rpc.OnLockScreenStatus;
+import com.smartdevicelink.proxy.rpc.OnPermissionsChange;
+import com.smartdevicelink.proxy.rpc.OnStreamRPC;
+import com.smartdevicelink.proxy.rpc.OnSystemRequest;
+import com.smartdevicelink.proxy.rpc.OnTBTClientState;
+import com.smartdevicelink.proxy.rpc.OnTouchEvent;
+import com.smartdevicelink.proxy.rpc.OnVehicleData;
+import com.smartdevicelink.proxy.rpc.PerformAudioPassThruResponse;
+import com.smartdevicelink.proxy.rpc.PerformInteractionResponse;
+import com.smartdevicelink.proxy.rpc.PutFileResponse;
+import com.smartdevicelink.proxy.rpc.ReadDIDResponse;
+import com.smartdevicelink.proxy.rpc.ResetGlobalPropertiesResponse;
+import com.smartdevicelink.proxy.rpc.ScrollableMessageResponse;
+import com.smartdevicelink.proxy.rpc.SendLocationResponse;
+import com.smartdevicelink.proxy.rpc.SetAppIconResponse;
+import com.smartdevicelink.proxy.rpc.SetDisplayLayoutResponse;
+import com.smartdevicelink.proxy.rpc.SetGlobalPropertiesResponse;
+import com.smartdevicelink.proxy.rpc.SetMediaClockTimerResponse;
+import com.smartdevicelink.proxy.rpc.ShowConstantTbtResponse;
+import com.smartdevicelink.proxy.rpc.ShowResponse;
+import com.smartdevicelink.proxy.rpc.SliderResponse;
+import com.smartdevicelink.proxy.rpc.SpeakResponse;
+import com.smartdevicelink.proxy.rpc.StreamRPCResponse;
+import com.smartdevicelink.proxy.rpc.SubscribeButtonResponse;
+import com.smartdevicelink.proxy.rpc.SubscribeVehicleDataResponse;
+import com.smartdevicelink.proxy.rpc.SystemRequestResponse;
+import com.smartdevicelink.proxy.rpc.UnsubscribeButtonResponse;
+import com.smartdevicelink.proxy.rpc.UnsubscribeVehicleDataResponse;
+import com.smartdevicelink.proxy.rpc.UpdateTurnListResponse;
+import com.smartdevicelink.proxy.rpc.enums.SdlDisconnectedReason;
+import com.smartdevicelink.proxy.rpc.listeners.OnRPCResponseListener;
+
+public class SdlManager implements IProxyListenerALM{
+
     private static SdlManager mInstance = new SdlManager();
+    private static SdlProxyALM mProxyALM;
+    private static SdlLifecycleListener mLifecycleListener;
+
+    private static SparseArray<OnRPCResponseListener> mNotificationListeners = new SparseArray<>();
 
     public static synchronized SdlManager getInstance() {
         return mInstance;
     }
 
     private SdlManager() {
+    }
+
+    void startSdlProxy(){
+
+    }
+
+    void stopSdlProxy(){
+        if(mProxyALM != null){
+            try {
+                mProxyALM.dispose();
+            } catch (SdlException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void addNotificationListener(FunctionID id, OnRPCResponseListener listener){
+        mNotificationListeners.append(id.getId(), listener);
+    }
+
+    public void setLifeCycleListener(SdlLifecycleListener listner){
+        mLifecycleListener = listner;
+    }
+
+    public void sendRpc(RPCRequest request){
+        if(mProxyALM != null){
+            try {
+                mProxyALM.sendRPCRequest(request);
+            } catch (SdlException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onProxyClosed(String info, Exception e, SdlDisconnectedReason reason) {
+
+    }
+
+    @Override
+    public void onError(String info, Exception e) {
+
+    }
+
+    // TODO: Don't think these are needed
+    @Override
+    public void onServiceEnded(OnServiceEnded serviceEnded) {
+
+    }
+
+    @Override
+    public void onServiceNACKed(OnServiceNACKed serviceNACKed) {
+
+    }
+
+    @Override
+    public void onServiceDataACK() {
+
+    }
+
+    // Everything below here is covered by OnRPCNotificationListener
+    @Override
+    public void onOnStreamRPC(OnStreamRPC notification) {
+
+    }
+
+    @Override
+    public void onOnHMIStatus(OnHMIStatus notification) {
+
+    }
+
+    @Override
+    public void onOnCommand(OnCommand notification) {
+
+    }
+
+    @Override
+    public void onOnButtonEvent(OnButtonEvent notification) {
+
+    }
+
+    @Override
+    public void onOnButtonPress(OnButtonPress notification) {
+
+    }
+
+    @Override
+    public void onOnLanguageChange(OnLanguageChange notification) {
+
+    }
+
+    @Override
+    public void onOnHashChange(OnHashChange notification) {
+
+    }
+
+    @Override
+    public void onOnPermissionsChange(OnPermissionsChange notification) {
+
+    }
+
+    @Override
+    public void onOnVehicleData(OnVehicleData notification) {
+
+    }
+
+    @Override
+    public void onOnAudioPassThru(OnAudioPassThru notification) {
+
+    }
+
+    @Override
+    public void onOnDriverDistraction(OnDriverDistraction notification) {
+
+    }
+
+    @Override
+    public void onOnTBTClientState(OnTBTClientState notification) {
+
+    }
+
+    @Override
+    public void onOnSystemRequest(OnSystemRequest notification) {
+
+    }
+
+    @Override
+    public void onOnKeyboardInput(OnKeyboardInput notification) {
+
+    }
+
+    @Override
+    public void onOnTouchEvent(OnTouchEvent notification) {
+
+    }
+
+    @Override
+    public void onOnLockScreenNotification(OnLockScreenStatus notification) {
+
+    }
+
+    // Everything below here is covered by OnRPCResponseListener
+    @Override
+    public void onStreamRPCResponse(StreamRPCResponse response) {
+
+    }
+
+    @Override
+    public void onGenericResponse(GenericResponse response) {
+
+    }
+
+    @Override
+    public void onAddCommandResponse(AddCommandResponse response) {
+
+    }
+
+    @Override
+    public void onAddSubMenuResponse(AddSubMenuResponse response) {
+
+    }
+
+    @Override
+    public void onCreateInteractionChoiceSetResponse(CreateInteractionChoiceSetResponse response) {
+
+    }
+
+    @Override
+    public void onAlertResponse(AlertResponse response) {
+
+    }
+
+    @Override
+    public void onDeleteCommandResponse(DeleteCommandResponse response) {
+
+    }
+
+    @Override
+    public void onDeleteInteractionChoiceSetResponse(DeleteInteractionChoiceSetResponse response) {
+
+    }
+
+    @Override
+    public void onDeleteSubMenuResponse(DeleteSubMenuResponse response) {
+
+    }
+
+    @Override
+    public void onPerformInteractionResponse(PerformInteractionResponse response) {
+
+    }
+
+    @Override
+    public void onResetGlobalPropertiesResponse(ResetGlobalPropertiesResponse response) {
+
+    }
+
+    @Override
+    public void onSetGlobalPropertiesResponse(SetGlobalPropertiesResponse response) {
+
+    }
+
+    @Override
+    public void onSetMediaClockTimerResponse(SetMediaClockTimerResponse response) {
+
+    }
+
+    @Override
+    public void onShowResponse(ShowResponse response) {
+
+    }
+
+    @Override
+    public void onSpeakResponse(SpeakResponse response) {
+
+    }
+
+    @Override
+    public void onSubscribeButtonResponse(SubscribeButtonResponse response) {
+
+    }
+
+    @Override
+    public void onUnsubscribeButtonResponse(UnsubscribeButtonResponse response) {
+
+    }
+
+    @Override
+    public void onSubscribeVehicleDataResponse(SubscribeVehicleDataResponse response) {
+
+    }
+
+    @Override
+    public void onUnsubscribeVehicleDataResponse(UnsubscribeVehicleDataResponse response) {
+
+    }
+
+    @Override
+    public void onGetVehicleDataResponse(GetVehicleDataResponse response) {
+
+    }
+
+    @Override
+    public void onPerformAudioPassThruResponse(PerformAudioPassThruResponse response) {
+
+    }
+
+    @Override
+    public void onEndAudioPassThruResponse(EndAudioPassThruResponse response) {
+
+    }
+
+    @Override
+    public void onPutFileResponse(PutFileResponse response) {
+
+    }
+
+    @Override
+    public void onDeleteFileResponse(DeleteFileResponse response) {
+
+    }
+
+    @Override
+    public void onListFilesResponse(ListFilesResponse response) {
+
+    }
+
+    @Override
+    public void onSetAppIconResponse(SetAppIconResponse response) {
+
+    }
+
+    @Override
+    public void onScrollableMessageResponse(ScrollableMessageResponse response) {
+
+    }
+
+    @Override
+    public void onChangeRegistrationResponse(ChangeRegistrationResponse response) {
+
+    }
+
+    @Override
+    public void onSetDisplayLayoutResponse(SetDisplayLayoutResponse response) {
+
+    }
+
+    @Override
+    public void onSliderResponse(SliderResponse response) {
+
+    }
+
+    @Override
+    public void onSystemRequestResponse(SystemRequestResponse response) {
+
+    }
+
+    @Override
+    public void onDiagnosticMessageResponse(DiagnosticMessageResponse response) {
+
+    }
+
+    @Override
+    public void onReadDIDResponse(ReadDIDResponse response) {
+
+    }
+
+    @Override
+    public void onGetDTCsResponse(GetDTCsResponse response) {
+
+    }
+
+    @Override
+    public void onDialNumberResponse(DialNumberResponse response) {
+
+    }
+
+    @Override
+    public void onSendLocationResponse(SendLocationResponse response) {
+
+    }
+
+    @Override
+    public void onShowConstantTbtResponse(ShowConstantTbtResponse response) {
+
+    }
+
+    @Override
+    public void onAlertManeuverResponse(AlertManeuverResponse response) {
+
+    }
+
+    @Override
+    public void onUpdateTurnListResponse(UpdateTurnListResponse response) {
+
     }
 }
