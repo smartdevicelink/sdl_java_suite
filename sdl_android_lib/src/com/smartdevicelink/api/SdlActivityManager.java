1d680e840baadc8ac53984d7eec45b2b1448cc6f
Mark Schwerzler <mschwerz@MGC000001540>
2016-04-05 12:33:30 -0400

    Stubbed the SdlActivityManager and the SdlActivity classes

--- sdl_android_lib/src/com/smartdevicelink/api/SdlActivityManager.java
+++ sdl_android_lib/src/com/smartdevicelink/api/SdlActivityManager.java
@@ -1,4 +1,64 @@
 package com.smartdevicelink.api;
 
-public class SdlActivityManager {
+import android.os.Bundle;
+
+import java.util.Stack;
+
+public class SdlActivityManager implements SdlLifecycleListener {
+
+    SdlActivity mCurrentActivity;
+    Stack<SdlActivity> mBackStack;
+
+
+    public SdlActivityManager(){
+
+    }
+
+    public void back(){
+
+    }
+
+    public void back(String name){
+
+    }
+
+
+    @Override
+    public void onSdlConnect() {
+
+    }
+
+    @Override
+    public void onSdlDisconnect() {
+
+    }
+
+    @Override
+    public void onBackground() {
+
+    }
+
+    @Override
+    public void onForeground() {
+
+    }
+
+    @Override
+    public void onExit() {
+
+    }
+
+    public void onSdlAppLaunch(SdlActivity mainActivity){
+
+    }
+
+    public void onSdlAppResume(Bundle savedActivityManager){
+
+    }
+
+    public void startSdlActivity(SdlActivity activity, int flags){
+
+    }
+
+
 }
