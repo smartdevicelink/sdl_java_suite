e7042cf14f784458f602932275cadfb8ed66429f
Mark Schwerzler <mschwerz@MGC000001540>
2016-04-06 12:46:00 -0400

    Fixed the interface for the onSdlAppLaunch and startSdlActivity

--- sdl_android_lib/src/com/smartdevicelink/api/SdlActivityManager.java
+++ sdl_android_lib/src/com/smartdevicelink/api/SdlActivityManager.java
@@ -7,13 +7,16 @@ import java.util.Stack;
 public class SdlActivityManager implements SdlLifecycleListener {
 
     SdlActivity mCurrentActivity;
-    Stack<SdlActivity> mBackStack;
+    Stack<SdlActivity> mBackStack = new Stack<>();
 
 
     public SdlActivityManager(){
 
     }
 
+    /**
+     * Removes the current activity from the backstack
+     */
     public void back(){
 
     }
@@ -48,15 +51,28 @@ public class SdlActivityManager implements SdlLifecycleListener {
 
     }
 
-    public void onSdlAppLaunch(SdlActivity mainActivity){
+    /**
+     * Creates the default SdlActivity based on the app coming into HMI FULL for the first time
+     * @param mainActivity Description of the derived SdlActivity class to be instantiated
+     */
+    public void onSdlAppLaunch(Class<? extends SdlActivity> mainActivity){
 
     }
 
+    /**
+     * Restores the SdlActivityManager to a previous point
+     * @param savedActivityManager serialized information needed to reconstruct the ActivityManager
+     */
     public void onSdlAppResume(Bundle savedActivityManager){
 
     }
 
-    public void startSdlActivity(SdlActivity activity, int flags){
+    /**
+     * Instantiates a new activity to be added to the backstack
+     * @param activity Description of the derived SdlActivity class to be instantiated
+     * @param flags Indication from the caller on how the backstack should behave
+     */
+    public void startSdlActivity(Class<? extends SdlActivity> activity, int flags){
 
     }
 
