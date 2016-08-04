bb8dd03786ba5b1b41bb6a237b7841b2c8b33bf9
Ty Strayer <tstrayer@ford.com>
2016-08-04 10:46:51 -0400

    Added getter for Sdl Message Version. Declared several SdlContext method implementations final.

--- sdl_android_lib/src/com/smartdevicelink/api/SdlApplication.java
+++ sdl_android_lib/src/com/smartdevicelink/api/SdlApplication.java
@@ -63,6 +63,7 @@ import com.smartdevicelink.proxy.rpc.PutFileResponse;
 import com.smartdevicelink.proxy.rpc.ReadDIDResponse;
 import com.smartdevicelink.proxy.rpc.ResetGlobalPropertiesResponse;
 import com.smartdevicelink.proxy.rpc.ScrollableMessageResponse;
+import com.smartdevicelink.proxy.rpc.SdlMsgVersion;
 import com.smartdevicelink.proxy.rpc.SendLocationResponse;
 import com.smartdevicelink.proxy.rpc.SetAppIconResponse;
 import com.smartdevicelink.proxy.rpc.SetDisplayLayoutResponse;
@@ -241,6 +242,11 @@ public class SdlApplication extends SdlContextAbsImpl {
     }
 
     @Override
+    public final void startSdlActivity(Class<? extends SdlActivity> activity, int flags) {
+        startSdlActivity(activity, null, flags);
+    }
+
+    @Override
     public final SdlFileManager getSdlFileManager() {
         return mSdlFileManager;
     }
@@ -267,7 +273,7 @@ public class SdlApplication extends SdlContextAbsImpl {
     }
 
     @Override
-    public void registerRpcNotificationListener(FunctionID functionID, OnRPCNotificationListener rpcNotificationListener) {
+    public final void registerRpcNotificationListener(FunctionID functionID, OnRPCNotificationListener rpcNotificationListener) {
         final int id = functionID.getId();
         HashSet<OnRPCNotificationListener> listenerSet = mNotificationListeners.get(id);
         if(listenerSet == null){
@@ -293,7 +299,7 @@ public class SdlApplication extends SdlContextAbsImpl {
     }
 
     @Override
-    public void unregisterRpcNotificationListener(FunctionID functionID, OnRPCNotificationListener rpcNotificationListener) {
+    public final void unregisterRpcNotificationListener(FunctionID functionID, OnRPCNotificationListener rpcNotificationListener) {
         int id = functionID.getId();
         HashSet<OnRPCNotificationListener> listenerSet = mNotificationListeners.get(id);
         if(listenerSet != null){
@@ -305,7 +311,7 @@ public class SdlApplication extends SdlContextAbsImpl {
     }
 
     @Override
-    public HMICapabilities getHmiCapabilities() {
+    public final HMICapabilities getHmiCapabilities() {
         if(mSdlProxyALM == null) return null;
         try {
             return mSdlProxyALM.getHmiCapabilities();
@@ -317,7 +323,7 @@ public class SdlApplication extends SdlContextAbsImpl {
     }
 
     @Override
-    public DisplayCapabilities getDisplayCapabilities() {
+    public final DisplayCapabilities getDisplayCapabilities() {
         if(mSdlProxyALM == null) return null;
         try {
             return mSdlProxyALM.getDisplayCapabilities();
@@ -329,12 +335,24 @@ public class SdlApplication extends SdlContextAbsImpl {
     }
 
     @Override
-    public VehicleType getVehicleType() {
+    public final VehicleType getVehicleType() {
         if(mSdlProxyALM == null) return null;
         try {
             return mSdlProxyALM.getVehicleType();
         } catch (SdlException e) {
             Log.e(TAG, "Unable to retrieve VehicleType");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public final SdlMsgVersion getSdlMessageVersion() {
+        if(mSdlProxyALM == null) return null;
+        try{
+            return mSdlProxyALM.getSdlMsgVersion();
+        } catch (SdlException e) {
+            Log.e(TAG, "Unable to retrieve SdlMessageVersion");
             e.printStackTrace();
             return null;
         }
