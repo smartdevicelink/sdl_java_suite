991d4070c57e6886cb3d97947e3f299038fa2402
mschwerzler <mschwerz@ford.com>
2016-10-21 16:31:21 -0400

    Added the DD state to the SdlContext

--- sdl_android_lib/src/com/smartdevicelink/api/SdlActivity.java
+++ sdl_android_lib/src/com/smartdevicelink/api/SdlActivity.java
@@ -18,6 +18,7 @@ import com.smartdevicelink.proxy.rpc.DisplayCapabilities;
 import com.smartdevicelink.proxy.rpc.HMICapabilities;
 import com.smartdevicelink.proxy.rpc.SdlMsgVersion;
 import com.smartdevicelink.proxy.rpc.VehicleType;
+import com.smartdevicelink.proxy.rpc.enums.DriverDistractionState;
 import com.smartdevicelink.proxy.rpc.enums.Language;
 import com.smartdevicelink.proxy.rpc.listeners.OnRPCNotificationListener;
 
@@ -270,6 +271,11 @@ public abstract class SdlActivity extends SdlContextAbsImpl {
     @Override
     public final Language getConnectedLanguage() {
         return getSdlApplicationContext().getConnectedLanguage();
+    }
+
+    @Override
+    public DriverDistractionState getCurrentDDState() {
+        return getSdlApplicationContext().getCurrentDDState();
     }
 
     public class SuperNotCalledException extends RuntimeException{
--- sdl_android_lib/src/com/smartdevicelink/api/interfaces/SdlContext.java
+++ sdl_android_lib/src/com/smartdevicelink/api/interfaces/SdlContext.java
@@ -16,6 +16,7 @@ import com.smartdevicelink.proxy.rpc.DisplayCapabilities;
 import com.smartdevicelink.proxy.rpc.HMICapabilities;
 import com.smartdevicelink.proxy.rpc.SdlMsgVersion;
 import com.smartdevicelink.proxy.rpc.VehicleType;
+import com.smartdevicelink.proxy.rpc.enums.DriverDistractionState;
 import com.smartdevicelink.proxy.rpc.enums.Language;
 import com.smartdevicelink.proxy.rpc.listeners.OnRPCNotificationListener;
 
@@ -60,5 +61,7 @@ public interface SdlContext {
     SdlMsgVersion getSdlMessageVersion();
 
     Language getConnectedLanguage();
+
+    DriverDistractionState getCurrentDDState();
 
 }
