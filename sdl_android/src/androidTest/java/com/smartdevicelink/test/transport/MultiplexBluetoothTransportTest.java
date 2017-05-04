61a72874261ffceb0d43b1935b9e528c029637c7
Austin Kirk <askirk@umich.edu>
2017-05-04 16:08:32 -0400

    Fixing which thread this runs on, and now can test with BT adapter

--- sdl_android/src/androidTest/java/com/smartdevicelink/test/transport/MultiplexBluetoothTransportTest.java
+++ sdl_android/src/androidTest/java/com/smartdevicelink/test/transport/MultiplexBluetoothTransportTest.java
@@ -1,14 +1,13 @@
 package com.smartdevicelink.test.transport;
 
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
 
 import com.smartdevicelink.transport.MultiplexBluetoothTransport;
 import com.smartdevicelink.transport.SdlRouterService;
 
 import junit.framework.TestCase;
-
-import static android.support.test.InstrumentationRegistry.getInstrumentation;
 
 
 public class MultiplexBluetoothTransportTest extends TestCase {
@@ -22,46 +21,43 @@ public class MultiplexBluetoothTransportTest extends TestCase {
 	Handler stateChangeHandler;
 	
 	public void testStateTransitions() {
+		if(Looper.myLooper() == null){
+			Looper.prepare();
+		}
 
-		getInstrumentation().runOnMainSync(new Runnable() {
+		stateChangeHandler = new Handler(){
+			int stateDesired = MultiplexBluetoothTransport.STATE_LISTEN;
 			@Override
-			public void run() {
-
-				stateChangeHandler = new Handler(){
-					int stateDesired = MultiplexBluetoothTransport.STATE_LISTEN;
-					@Override
-					public void handleMessage(Message msg) {
-						if(!isWaitingForResponse){
-							return;
+			public void handleMessage(Message msg) {
+				if(!isWaitingForResponse){
+					return;
+				}
+				switch(msg.what){
+					case SdlRouterService.MESSAGE_STATE_CHANGE:
+						if(msg.arg1 == stateDesired){
+							didCorrectThing = true;
+							break;
 						}
-						switch(msg.what){
-							case SdlRouterService.MESSAGE_STATE_CHANGE:
-								if(msg.arg1 == stateDesired){
-									didCorrectThing = true;
-									break;
-								}
-							default:
-								didCorrectThing = false;
-						}
-						REQUEST_LOCK.notify();
-					}
-
-				};
-
-				//TODO test for more than the two states
-				bluetooth = MultiplexBluetoothTransport.getBluetoothSerialServerInstance();
-				assertNull(bluetooth);
-
-				bluetooth = MultiplexBluetoothTransport.getBluetoothSerialServerInstance(stateChangeHandler);
-				assertEquals(bluetooth.getState(), MultiplexBluetoothTransport.STATE_NONE);
-
-				//TODO test changing to a different state without having Bluetooth permissions
-
-				bluetooth.stop();
-				assertEquals(bluetooth.getState(), MultiplexBluetoothTransport.STATE_NONE);
-
+					default:
+						didCorrectThing = false;
+				}
+				REQUEST_LOCK.notify();
 			}
-		});
+
+		};
+
+		//TODO test for more than the two states
+		bluetooth = MultiplexBluetoothTransport.getBluetoothSerialServerInstance();
+		assertNull(bluetooth);
+
+		bluetooth = MultiplexBluetoothTransport.getBluetoothSerialServerInstance(stateChangeHandler);
+		assertEquals(bluetooth.getState(), MultiplexBluetoothTransport.STATE_NONE);
+
+		bluetooth.start();
+		assertEquals(bluetooth.getState(), MultiplexBluetoothTransport.STATE_LISTEN);
+
+		bluetooth.stop();
+		assertEquals(bluetooth.getState(), MultiplexBluetoothTransport.STATE_NONE);
 	}
 	
 	private void notifyResponseReceived(){
