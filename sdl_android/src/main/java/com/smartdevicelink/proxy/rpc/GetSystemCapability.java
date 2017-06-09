0d98ba2f617dabb130128c96a89a893f2b07fee0
Austin Kirk <askirk@umich.edu>
2017-06-09 10:52:43 -0400

    Adding Features from Evolution Proposal
    -Edited HMICapabilities to include video&audio streaming params
    -Added GetSystemCapability Request & Response classes
    -Added SystemCapability struct
    - Added SystemCapabilityType enum

---
+++ sdl_android/src/main/java/com/smartdevicelink/proxy/rpc/GetSystemCapability.java
@@ -0,0 +1,48 @@
+package com.smartdevicelink.proxy.rpc;
+
+import com.smartdevicelink.protocol.enums.FunctionID;
+import com.smartdevicelink.proxy.RPCRequest;
+import com.smartdevicelink.proxy.rpc.enums.SystemCapabilityType;
+
+import java.util.Hashtable;
+
+/**
+ * Used to request the corresponding capability object for a given capability.
+ */
+
+public class GetSystemCapability extends RPCRequest {
+    public static final String KEY_SYSTEM_CAPABILITY_TYPE = "systemCapabilityType";
+
+    /**
+     * Constructs a new GetSystemCapability object
+     */
+    public GetSystemCapability(){
+        super(FunctionID.GET_SYSTEM_CAPABILITY.toString());
+    }
+
+    /**
+     * <p>Constructs a new GetSystemCapability object indicated by the Hashtable parameter</p>
+     *
+     * @param hash
+     *            The Hashtable to use
+     */
+    public GetSystemCapability(Hashtable<String, Object> hash) {
+        super(hash);
+    }
+
+    /**
+     * Used to get the SystemCapabilityType being requested
+     * @return the SystemCapabilityType being requested
+     */
+    public SystemCapabilityType getSystemCapabilityType(){
+        return (SystemCapabilityType) getObject(SystemCapabilityType.class, KEY_SYSTEM_CAPABILITY_TYPE);
+    }
+
+    /**
+     * Used to set the SystemCapabilityType being requested
+     * @param value SystemCapabilityType being requested
+     */
+    public void setSystemCapabilityType(SystemCapabilityType value){
+        setParameters(KEY_SYSTEM_CAPABILITY_TYPE, value);
+    }
+}
---
+++ sdl_android/src/main/java/com/smartdevicelink/proxy/rpc/SystemCapability.java
@@ -0,0 +1,41 @@
+package com.smartdevicelink.proxy.rpc;
+
+import com.smartdevicelink.proxy.RPCStruct;
+import com.smartdevicelink.proxy.rpc.enums.SystemCapabilityType;
+
+import java.util.Hashtable;
+
+/**
+ * Struct that indicates the a SystemCapabilityType and houses different structs to describe particular capabilities
+ */
+
+public class SystemCapability extends RPCStruct {
+    public static final String KEY_SYSTEM_CAPABILITY_TYPE = "systemCapabilityType";
+    public static final String KEY_NAVIGATION_CAPABILITY = "navigationCapability";
+    public static final String KEY_PHONE_CAPABILITY = "phoneCapability";
+    public static final String KEY_VIDEO_STREAMING_CAPABILITY = "videoStreamingCapability";
+    public static final String KEY_AUDIO_STREAMING_CAPABILITY = "audioStreamingCapability";
+
+    public SystemCapability(){ }
+
+    public SystemCapability(Hashtable<String, Object> hash) {
+        super(hash);
+    }
+
+    /**
+     *
+     * @return The SystemCapabilityType that indicates which type of data should be changed and identifies which data object exists in this struct. For example, if the SystemCapability Type is NAVIGATION then a "navigationCapability" should exist
+     */
+    public SystemCapabilityType getSystemCapabilityType(){
+        return (SystemCapabilityType) getObject(SystemCapabilityType.class, KEY_SYSTEM_CAPABILITY_TYPE);
+    }
+
+    /**
+     * @param value Set the SystemCapabilityType that indicates which type of data should be changed and identifies which data object exists in this struct.
+     */
+    public void setSystemCapabilityType(SystemCapabilityType value){
+        setValue(KEY_SYSTEM_CAPABILITY_TYPE, value);
+    }
+
+    // TODO: Implement individual capability methods
+}
