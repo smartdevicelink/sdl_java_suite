33ad6fd374f7cc46808dbe13fd8c91f81ca38da6
Mat Wszedybyl <mat@livio.io>
2017-07-13 10:20:29 -0400

    Fixed TransportBroker Unit test for SendMessageToRouterServiceException()

--- sdl_android/src/test/java/com/smartdevicelink/transport/TransportBrokerTests.java
+++ sdl_android/src/test/java/com/smartdevicelink/transport/TransportBrokerTests.java
@@ -3,13 +3,10 @@ package com.smartdevicelink.transport;
 import android.content.Context;
 import android.os.Message;
 import android.os.Messenger;
-import android.os.TransactionTooLargeException;
 
 import junit.framework.TestCase;
 
 import org.mockito.Mock;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -40,40 +37,21 @@ public class TransportBrokerTests extends TestCase{
 
 
 	public void testSendMessageToRouterServiceException() throws Exception{
-		TransportBroker broker = spy(new TransportBroker(mMockContext, TEST_APP_ID ,rsvp.getService()));
-//		Messenger mockMessenger = mock(Messenger.class, new Answer<Object>() {
-//			@Override
-//			public Object answer(InvocationOnMock invocation) throws Throwable {
-////				int count = 0;
-////				String methodName = invocation.getMethod().getName();
-////				if (count<1 && methodName=="send") {
-////					count++;
-//						throw new TransactionTooLargeException();
-////				} else {
-////					throw new NullPointerException();
-////				}
-//
-//			}
-//		});
 
-		Messenger mockMessenger = mock(Messenger.class);
-		doThrow(TransactionTooLargeException.class).when(mockMessenger).send(any(Message.class));
-		broker.routerServiceMessenger = mockMessenger;
+		TransportBroker broker = spy(new TransportBroker(mMockContext, TEST_APP_ID ,rsvp.getService()));
 		Message message = new Message();
 		message.what = TransportConstants.ROUTER_REGISTER_CLIENT;
 		broker.registeredWithRouterService = true;
 		broker.isBound = true;
 
-		boolean result = true;
-		try {
-			 result = broker.sendMessageToRouterService(message);
-		} catch(Exception e){
-
-		}
+		Messenger mockMessenger = mock(Messenger.class);
+		doThrow(new NullPointerException()).when(mockMessenger).send(message);
+		broker.routerServiceMessenger = mockMessenger;
+		boolean result = broker.sendMessageToRouterService(message);
 
 		assertFalse(result);
 		verify(mockMessenger).send(any(Message.class));
-		verify(broker, times(3)).sendMessageToRouterService(any(Message.class), anyInt());
+		verify(broker, times(1)).sendMessageToRouterService(any(Message.class), anyInt());
 	}
 
 
