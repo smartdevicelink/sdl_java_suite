411d7ffa7950f636c475b3828fe7c39442aa2ff4
Joey Grover <joeygrover@gmail.com>
2017-08-28 13:41:46 -0400

    Fixes #597 isEmulator

--- sdl_android/src/androidTest/java/com/smartdevicelink/test/util/DeviceUtil.java
+++ sdl_android/src/androidTest/java/com/smartdevicelink/test/util/DeviceUtil.java
@@ -1,6 +1,7 @@
 package com.smartdevicelink.test.util;
 
 import android.os.Build;
+import android.util.Log;
 
 public class DeviceUtil {
     public static boolean isEmulator() {
@@ -8,7 +9,10 @@ public class DeviceUtil {
                 || Build.FINGERPRINT.startsWith("unknown")
                 || Build.MODEL.contains("google_sdk")
                 || Build.MODEL.contains("Emulator")
+                || Build.MODEL.contains("Android SDL built for")
                 || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+                || (Build.BRAND.startsWith("Android") && Build.DEVICE.startsWith("generic"))
+                || (Build.PRODUCT != null && Build.PRODUCT.startsWith("sdk_google_phone"))
                 || "google_sdk".equals(Build.PRODUCT);
     }
 }
