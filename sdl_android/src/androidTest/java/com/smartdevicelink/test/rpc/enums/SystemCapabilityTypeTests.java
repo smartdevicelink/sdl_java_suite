aa4efc95112543d78ef06ff1345c7a5706997e0d
Anil  Dahiya <Anil_Dahiya@ad.infosys.com>
2017-09-27 11:44:22 +0530

    Updating the SystemCapabilityTypeTests for including additional parameters addeded to SystemCapabilityType.

--- sdl_android/src/androidTest/java/com/smartdevicelink/test/rpc/enums/SystemCapabilityTypeTests.java
+++ sdl_android/src/androidTest/java/com/smartdevicelink/test/rpc/enums/SystemCapabilityTypeTests.java
@@ -26,11 +26,38 @@ public class SystemCapabilityTypeTests extends TestCase {
 		SystemCapabilityType enumVideoStreaming = SystemCapabilityType.valueForString(example);
 		example = "REMOTE_CONTROL";
 		SystemCapabilityType enumRemoteControl = SystemCapabilityType.valueForString(example);
+		example = "HMI";
+		SystemCapabilityType enumHmi = SystemCapabilityType.valueForString(example);
+		example = "DISPLAY";
+		SystemCapabilityType enumDisplay = SystemCapabilityType.valueForString(example);
+		example = "AUDIO_PASSTHROUGH";
+		SystemCapabilityType enumAudioPassThrough = SystemCapabilityType.valueForString(example);
+		example = "BUTTON";
+		SystemCapabilityType enumButton = SystemCapabilityType.valueForString(example);
+		example = "HMI_ZONE";
+		SystemCapabilityType enumHmiZone = SystemCapabilityType.valueForString(example);
+		example = "PRESET_BANK";
+		SystemCapabilityType enumPresetBank = SystemCapabilityType.valueForString(example);
+		example = "SOFTBUTTON";
+		SystemCapabilityType enumSoftButton = SystemCapabilityType.valueForString(example);
+		example = "SPEECH";
+		SystemCapabilityType enumSpeech = SystemCapabilityType.valueForString(example);
+		example = "VOICE_RECOGNITION";
+		SystemCapabilityType enumVoiceRecognition = SystemCapabilityType.valueForString(example);
 
 		assertNotNull("NAVIGATION returned null", enumNavigation);
 		assertNotNull("PHONE_CALL returned null", enumPhoneCall);
 		assertNotNull("VIDEO_STREAMING returned null", enumVideoStreaming);
 		assertNotNull("REMOTE_CONTROL returned null", enumRemoteControl);
+		assertNotNull("HMI returned null", enumHmi);
+		assertNotNull("DISPLAY returned null", enumDisplay);
+		assertNotNull("AUDIO_PASSTHROUGH returned null", enumAudioPassThrough);
+		assertNotNull("BUTTON returned null", enumButton);
+		assertNotNull("HMI_ZONE returned null", enumHmiZone);
+		assertNotNull("PRESET_BANK returned null", enumPresetBank);
+		assertNotNull("SOFTBUTTON returned null", enumSoftButton);
+		assertNotNull("SPEECH returned null", enumSpeech);
+		assertNotNull("VOICE_RECOGNITION returned null", enumVoiceRecognition);
 	}
 
 	/**
@@ -72,6 +99,15 @@ public class SystemCapabilityTypeTests extends TestCase {
 		enumTestList.add(SystemCapabilityType.PHONE_CALL);
 		enumTestList.add(SystemCapabilityType.VIDEO_STREAMING);
 		enumTestList.add(SystemCapabilityType.REMOTE_CONTROL);
+		enumTestList.add(SystemCapabilityType.HMI);
+		enumTestList.add(SystemCapabilityType.DISPLAY);
+		enumTestList.add(SystemCapabilityType.AUDIO_PASSTHROUGH);
+		enumTestList.add(SystemCapabilityType.BUTTON);
+		enumTestList.add(SystemCapabilityType.HMI_ZONE);
+		enumTestList.add(SystemCapabilityType.PRESET_BANK);
+		enumTestList.add(SystemCapabilityType.SOFTBUTTON);
+		enumTestList.add(SystemCapabilityType.SPEECH);
+		enumTestList.add(SystemCapabilityType.VOICE_RECOGNITION);
 
 		assertTrue("Enum value list does not match enum class list", 
 				enumValueList.containsAll(enumTestList) && enumTestList.containsAll(enumValueList));
