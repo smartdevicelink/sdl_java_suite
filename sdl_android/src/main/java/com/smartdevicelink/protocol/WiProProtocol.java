3d5c8a01017b68f512987046cacd55b595fd6110
Tuan Nguyen <tnguy238@ford.com>
2018-02-12 14:15:53 -0800

    #467 Add null check before accessing message data

--- sdl_android/src/main/java/com/smartdevicelink/protocol/WiProProtocol.java
+++ sdl_android/src/main/java/com/smartdevicelink/protocol/WiProProtocol.java
@@ -231,58 +231,60 @@ public class WiProProtocol extends AbstractProtocol {
 		}
 		
 		synchronized(messageLock) {
-			if (data.length > getMtu(sessionType)) {
+			if (data != null) {
+				if (data.length > getMtu(sessionType)) {
 
-				messageID++;
-	
-				// Assemble first frame.
-				Long mtu = getMtu(sessionType);
-				int frameCount = new Long(data.length / mtu).intValue();
-				if (data.length % mtu > 0) {
-					frameCount++;
-				}
-				//byte[] firstFrameData = new byte[HEADER_SIZE];
-				byte[] firstFrameData = new byte[8];
-				// First four bytes are data size.
-				System.arraycopy(BitConverter.intToByteArray(data.length), 0, firstFrameData, 0, 4);
-				// Second four bytes are frame count.
-				System.arraycopy(BitConverter.intToByteArray(frameCount), 0, firstFrameData, 4, 4);
-
-				SdlPacket firstHeader = SdlPacketFactory.createMultiSendDataFirst(sessionType, sessionID, messageID, getMajorVersionByte(),firstFrameData,protocolMsg.getPayloadProtected());
-				firstHeader.setPriorityCoefficient(1+protocolMsg.priorityCoefficient);
-				//Send the first frame
-				handlePacketToSend(firstHeader);
-				
-				int currentOffset = 0;
-				byte frameSequenceNumber = 0;
-
-				for (int i = 0; i < frameCount; i++) {
-					if (i < (frameCount - 1)) {
-	                     ++frameSequenceNumber;
-	                        if (frameSequenceNumber ==
-	                                SdlPacket.FRAME_INFO_FINAL_CONNESCUTIVE_FRAME) {
-	                            // we can't use 0x00 as frameSequenceNumber, because
-	                            // it's reserved for the last frame
-	                            ++frameSequenceNumber;
-	                        }
-					} else {
-						frameSequenceNumber = SdlPacket.FRAME_INFO_FINAL_CONNESCUTIVE_FRAME;
-					} // end-if
-					
-					int bytesToWrite = data.length - currentOffset;
-					if (bytesToWrite > mtu) {
-						bytesToWrite = mtu.intValue();
+					messageID++;
+		
+					// Assemble first frame.
+					Long mtu = getMtu(sessionType);
+					int frameCount = new Long(data.length / mtu).intValue();
+					if (data.length % mtu > 0) {
+						frameCount++;
 					}
-					SdlPacket consecHeader = SdlPacketFactory.createMultiSendDataRest(sessionType, sessionID, bytesToWrite, frameSequenceNumber , messageID, getMajorVersionByte(),data, currentOffset, bytesToWrite, protocolMsg.getPayloadProtected());
-					consecHeader.setPriorityCoefficient(i+2+protocolMsg.priorityCoefficient);
-					handlePacketToSend(consecHeader);
-					currentOffset += bytesToWrite;
+					//byte[] firstFrameData = new byte[HEADER_SIZE];
+					byte[] firstFrameData = new byte[8];
+					// First four bytes are data size.
+					System.arraycopy(BitConverter.intToByteArray(data.length), 0, firstFrameData, 0, 4);
+					// Second four bytes are frame count.
+					System.arraycopy(BitConverter.intToByteArray(frameCount), 0, firstFrameData, 4, 4);
+
+					SdlPacket firstHeader = SdlPacketFactory.createMultiSendDataFirst(sessionType, sessionID, messageID, getMajorVersionByte(),firstFrameData,protocolMsg.getPayloadProtected());
+					firstHeader.setPriorityCoefficient(1+protocolMsg.priorityCoefficient);
+					//Send the first frame
+					handlePacketToSend(firstHeader);
+					
+					int currentOffset = 0;
+					byte frameSequenceNumber = 0;
+
+					for (int i = 0; i < frameCount; i++) {
+						if (i < (frameCount - 1)) {
+		                     ++frameSequenceNumber;
+		                        if (frameSequenceNumber ==
+		                                SdlPacket.FRAME_INFO_FINAL_CONNESCUTIVE_FRAME) {
+		                            // we can't use 0x00 as frameSequenceNumber, because
+		                            // it's reserved for the last frame
+		                            ++frameSequenceNumber;
+		                        }
+						} else {
+							frameSequenceNumber = SdlPacket.FRAME_INFO_FINAL_CONNESCUTIVE_FRAME;
+						} // end-if
+						
+						int bytesToWrite = data.length - currentOffset;
+						if (bytesToWrite > mtu) {
+							bytesToWrite = mtu.intValue();
+						}
+						SdlPacket consecHeader = SdlPacketFactory.createMultiSendDataRest(sessionType, sessionID, bytesToWrite, frameSequenceNumber , messageID, getMajorVersionByte(),data, currentOffset, bytesToWrite, protocolMsg.getPayloadProtected());
+						consecHeader.setPriorityCoefficient(i+2+protocolMsg.priorityCoefficient);
+						handlePacketToSend(consecHeader);
+						currentOffset += bytesToWrite;
+					}
+				} else {
+					messageID++;
+					SdlPacket header = SdlPacketFactory.createSingleSendData(sessionType, sessionID, data.length, messageID, getMajorVersionByte(),data, protocolMsg.getPayloadProtected());
+					header.setPriorityCoefficient(protocolMsg.priorityCoefficient);
+					handlePacketToSend(header);
 				}
-			} else {
-				messageID++;
-				SdlPacket header = SdlPacketFactory.createSingleSendData(sessionType, sessionID, data.length, messageID, getMajorVersionByte(),data, protocolMsg.getPayloadProtected());
-				header.setPriorityCoefficient(protocolMsg.priorityCoefficient);
-				handlePacketToSend(header);
 			}
 		}
 	}
