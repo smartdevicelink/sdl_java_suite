ba33d0d4863f606285a845ff68fd09c3bc214df6
Tuan Nguyen <tnguy238@ford.com>
2018-03-06 11:43:13 -0800

    #688 Add missing import

--- sdl_android/src/main/java/com/smartdevicelink/transport/SdlRouterStatusProvider.java
+++ sdl_android/src/main/java/com/smartdevicelink/transport/SdlRouterStatusProvider.java
@@ -18,6 +18,7 @@ import android.os.Message;
 import android.os.Messenger;
 import android.os.RemoteException;
 import android.util.Log;
+import android.os.Looper;
 
 public class SdlRouterStatusProvider {
 
