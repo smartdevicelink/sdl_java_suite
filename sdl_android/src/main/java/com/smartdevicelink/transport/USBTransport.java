dad4736293b321b01f6a06c980c77999b71dda46
Joey Grover <joeygrover@gmail.com>
2018-03-09 13:30:13 -0500

    Prevent proxy cycle from happening when USB is disconnected

--- sdl_android/src/main/java/com/smartdevicelink/transport/USBTransport.java
+++ sdl_android/src/main/java/com/smartdevicelink/transport/USBTransport.java
@@ -199,9 +199,7 @@ public class USBTransport extends SdlTransport {
     /**
      * Sends the array of bytes over USB.
      *
-     * @param msgBytes Array of bytes to send
-     * @param offset   Offset in the array to start from
-     * @param length   Number of bytes to send
+     * @param packet The packet that is to be written out on the USB transport
      * @return true if the bytes are sent successfully
      */
     @Override
@@ -420,6 +418,12 @@ public class USBTransport extends SdlTransport {
                 String disconnectMsg = (msg == null ? "" : msg);
                 if (ex != null) {
                     disconnectMsg += ", " + ex.toString();
+
+                    if(ex instanceof SdlException
+                            && SdlExceptionCause.SDL_USB_PERMISSION_DENIED.equals(((SdlException)ex).getSdlExceptionCause())){
+                        //If the USB device disconnected we don't want to cycle the proxy ourselves
+                        ex = null;
+                    }
                 }
 
                 if (ex == null) {
