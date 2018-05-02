31ac2dd40a01282adbd5e89cf386bfc5559944a9
Bilal <bilal@Bilals-MBP.localdomain>
2018-05-02 13:25:00 -0400

    Added JSON file to test OnSeekMediaClockTimer

--- sdl_android/src/androidTest/java/com/smartdevicelink/test/rpc/notifications/OnSeekMediaClockTimerTests.java
+++ sdl_android/src/androidTest/java/com/smartdevicelink/test/rpc/notifications/OnSeekMediaClockTimerTests.java
@@ -1,14 +1,20 @@
 package com.smartdevicelink.test.rpc.notifications;
 
+import com.smartdevicelink.marshal.JsonRPCMarshaller;
 import com.smartdevicelink.protocol.enums.FunctionID;
 import com.smartdevicelink.proxy.RPCMessage;
 import com.smartdevicelink.proxy.rpc.OnSeekMediaClockTimer;
 import com.smartdevicelink.proxy.rpc.StartTime;
 import com.smartdevicelink.test.BaseRpcTests;
+import com.smartdevicelink.test.JsonUtils;
 import com.smartdevicelink.test.Test;
+import com.smartdevicelink.test.Validator;
+import com.smartdevicelink.test.json.rpc.JsonFileReader;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.Hashtable;
 
 
 public class OnSeekMediaClockTimerTests extends BaseRpcTests {
@@ -58,5 +64,32 @@ public class OnSeekMediaClockTimerTests extends BaseRpcTests {
         testNullBase(msg);
 
         assertNull(Test.NULL, msg.getSeekTime());
+    }
+
+    /**
+     * Tests a valid JSON construction of this RPC message.
+     */
+    public void testJsonConstructor () {
+        JSONObject commandJson = JsonFileReader.readId(this.mContext, getCommandType(), getMessageType());
+        assertNotNull(Test.NOT_NULL, commandJson);
+
+        try {
+            Hashtable<String, Object> hash = JsonRPCMarshaller.deserializeJSONObject(commandJson);
+            OnSeekMediaClockTimer cmd = new OnSeekMediaClockTimer(hash);
+
+            JSONObject body = JsonUtils.readJsonObjectFromJsonObject(commandJson, getMessageType());
+            assertNotNull(Test.NOT_NULL, body);
+
+            // Test everything in the json body.
+            assertEquals(Test.MATCH, JsonUtils.readStringFromJsonObject(body, RPCMessage.KEY_FUNCTION_NAME), cmd.getFunctionName());
+
+            JSONObject parameters = JsonUtils.readJsonObjectFromJsonObject(body, RPCMessage.KEY_PARAMETERS);
+            JSONObject seekTime = JsonUtils.readJsonObjectFromJsonObject(parameters, OnSeekMediaClockTimer.KEY_SEEK_TIME);
+            StartTime referenceSeekTime = new StartTime(JsonRPCMarshaller.deserializeJSONObject(seekTime));
+            assertTrue(Test.TRUE, Validator.validateStartTime(referenceSeekTime, cmd.getSeekTime()));
+
+        } catch (JSONException e) {
+            fail(Test.JSON_FAIL);
+        }
     }
 }
