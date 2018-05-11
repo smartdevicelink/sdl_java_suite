9f4299bd6ab16367caf21544bd22006d60352dec
Joey Grover <joeygrover@gmail.com>
2018-05-11 11:26:15 -0400

    Track active transports for protocol < 5

--- sdl_android/src/main/java/com/smartdevicelink/protocol/WiProProtocol.java
+++ sdl_android/src/main/java/com/smartdevicelink/protocol/WiProProtocol.java
@@ -599,7 +599,21 @@ public class WiProProtocol extends AbstractProtocol {
 							session.setAcceptedVideoParams(agreedVideoParams);
 						}
 					}
-				}else{
+				}else{ //TODO check if we should set protocol version here
+					TransportType transportType = packet.getTransportType();
+					if(requiresHighBandwidth
+							&& TransportType.BLUETOOTH.equals(transportType)){
+						//transport can't support high bandwidth
+						onTransportNotAccepted(transportType + "can't support high bandwidth requirement, and secondary transport not supported in this protocol version");
+						return;
+					}
+					//If version < 5 and transport is acceptable we need to just add these
+					activeTransports.put(SessionType.RPC, transportType);
+					activeTransports.put(SessionType.BULK_DATA, transportType);
+					activeTransports.put(SessionType.CONTROL, transportType);
+					activeTransports.put(SessionType.NAV, transportType);
+					activeTransports.put(SessionType.PCM, transportType);
+
 					if (protocolVersion.getMajor() > 1){
 						if (packet.payload!= null && packet.dataSize == 4){ //hashid will be 4 bytes in length
 							hashID = BitConverter.intFromByteArray(packet.payload, 0);
