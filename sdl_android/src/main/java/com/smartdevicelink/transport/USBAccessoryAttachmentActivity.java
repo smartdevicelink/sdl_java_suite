0ba9f5b4f57f966e4f603bd8878647b42e68718f
Joey Grover <joeygrover@gmail.com>
2018-11-27 19:43:31 -0500

    Remove double intent check in usb activity

--- sdl_android/src/main/java/com/smartdevicelink/transport/USBAccessoryAttachmentActivity.java
+++ sdl_android/src/main/java/com/smartdevicelink/transport/USBAccessoryAttachmentActivity.java
@@ -98,7 +98,6 @@ public class USBAccessoryAttachmentActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        checkUsbAccessoryIntent("Create");
     }
 
     @Override
@@ -107,7 +106,16 @@ public class USBAccessoryAttachmentActivity extends Activity {
         checkUsbAccessoryIntent("Resume");
     }
 
-    private void checkUsbAccessoryIntent(String sourceAction) {
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        this.setIntent(intent);
+    }
+
+    private synchronized void checkUsbAccessoryIntent(String sourceAction) {
+        if(usbAccessory != null){
+            return;
+        }
         final Intent intent = getIntent();
         String action = intent.getAction();
         Log.d(TAG, sourceAction + " with action: " + action);
