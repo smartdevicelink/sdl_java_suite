1dfe2690a0d462fc1d6c1b4906f9a34ef9c296a5
BrettyWhite <geekman3454@protonmail.com>
2019-02-08 09:52:48 -0500

    app service data - needs tests

---
+++ sdl_android/src/main/java/com/smartdevicelink/proxy/rpc/AppServiceData.java
@@ -0,0 +1,93 @@
+package com.smartdevicelink.proxy.rpc;
+
+import android.support.annotation.NonNull;
+
+import com.smartdevicelink.proxy.RPCStruct;
+import com.smartdevicelink.proxy.rpc.enums.AppServiceType;
+
+import java.util.Hashtable;
+
+/**
+ * Contains all the current data of the app service. The serviceType will link to which of the
+ * service data objects are included in this object. (eg if service type equals MEDIA, the
+ * mediaServiceData param should be included.
+ */
+public class AppServiceData extends RPCStruct {
+
+	public static final String KEY_SERVICE_TYPE = "serviceType";
+	public static final String KEY_SERVICE_ID   = "serviceId";
+	public static final String KEY_MEDIA_SERVICE_DATA = "mediaServiceData";
+	public static final String KEY_WEATHER_SERVICE_DATA = "weatherServiceData";
+
+	// Constructors
+	public AppServiceData() { }
+
+	public AppServiceData(Hashtable<String, Object> hash) {
+		super(hash);
+	}
+
+	public AppServiceData(@NonNull AppServiceType serviceType, @NonNull String serviceId) {
+		this();
+		setServiceType(serviceType);
+		setServiceId(serviceId);
+	}
+
+	// Setters and Getters
+
+	/**
+	 * @param serviceType -
+	 */
+	public void setServiceType(@NonNull AppServiceType serviceType) {
+		setValue(KEY_SERVICE_TYPE, serviceType);
+	}
+
+	/**
+	 * @return serviceType -
+	 */
+	public AppServiceType getServiceType() {
+		return (AppServiceType) getObject(AppServiceType.class,KEY_SERVICE_TYPE);
+	}
+
+	/**
+	 * @param serviceId -
+	 */
+	public void setServiceId(@NonNull String serviceId) {
+		setValue(KEY_SERVICE_ID, serviceId);
+	}
+
+	/**
+	 * @return serviceId -
+	 */
+	public String getServiceId() {
+		return getString(KEY_SERVICE_ID);
+	}
+
+	/**
+	 * @param mediaServiceData -
+	 */
+	public void setMediaServiceData(MediaServiceData mediaServiceData) {
+		setValue(KEY_MEDIA_SERVICE_DATA, mediaServiceData);
+	}
+
+	/**
+	 * @return mediaServiceData -
+	 */
+	public MediaServiceData getMediaServiceData() {
+		return (MediaServiceData) getObject(MediaServiceData.class,KEY_MEDIA_SERVICE_DATA);
+	}
+
+	/**
+	 * @param weatherServiceData -
+	 */
+	public void setWeatherServiceData(WeatherServiceData weatherServiceData) {
+		setValue(KEY_WEATHER_SERVICE_DATA, weatherServiceData);
+	}
+
+	/**
+	 * @return weatherServiceData -
+	 */
+	public WeatherServiceData getWeatherServiceData() {
+		return (WeatherServiceData) getObject(WeatherServiceData.class,KEY_WEATHER_SERVICE_DATA);
+	}
+
+}
