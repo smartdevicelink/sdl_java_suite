0c8601c6e0e350353814d0d9a632de5a48be86a8
BrettyWhite <geekman3454@protonmail.com>
2019-02-12 10:40:00 -0500

    PerformAppServiceInteractionResponse and tests

---
+++ sdl_android/src/androidTest/java/com/smartdevicelink/test/rpc/responses/PerformAppServiceInteractionResponseTests.java
@@ -0,0 +1,7 @@
+package com.smartdevicelink.test.rpc.responses;
+
+/**
+ * Created by brettywhite on 2/12/19.
+ */
+public class PerformAppServiceInteractionResponseTests {
+}
---
+++ sdl_android/src/main/java/com/smartdevicelink/proxy/rpc/PerformAppServiceInteractionResponse.java
@@ -0,0 +1,7 @@
+package com.smartdevicelink.proxy.rpc;
+
+/**
+ * Created by brettywhite on 2/12/19.
+ */
+public class PerformAppServiceInteractionResponse {
+}
