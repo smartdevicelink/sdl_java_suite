b16f9091ea1ea0949196288d3be599915e66c9f0
Bilal Alsharifi <bilal.alsharifi@gmail.com>
2019-02-27 12:47:21 -0500

    Remove unused code from RPCRequest.java

--- sdl_android/src/main/java/com/smartdevicelink/proxy/RPCRequest.java
+++ sdl_android/src/main/java/com/smartdevicelink/proxy/RPCRequest.java
@@ -17,14 +17,6 @@ public class RPCRequest extends RPCMessage {
 		messageType = RPCMessage.KEY_REQUEST;
 	}
 
-	/**
-	 *<p>Constructs a newly allocated RPCRequest object using a RPCRequest object</p>
-	 *@param rpcMsg The {@linkplain RPCMessage} to use
-	 */
-	public RPCRequest(RPCMessage rpcMsg) {
-		super(preprocessMsg(rpcMsg));
-	}
-
 	public RPCRequest(Hashtable<String, Object> hash) {
 		super(hash);
 	}
@@ -32,14 +24,6 @@ public class RPCRequest extends RPCMessage {
 	public RPCRequest(RPCRequest request){
 		super(request);
 		setCorrelationID(CorrelationIdGenerator.generateId());
-	}
-
-	static RPCMessage preprocessMsg (RPCMessage rpcMsg) {
-		if (rpcMsg.getMessageType() != RPCMessage.KEY_REQUEST) {
-			rpcMsg.messageType = RPCMessage.KEY_REQUEST;
-		}
-
-		return rpcMsg;
 	}
 
 	public Integer getCorrelationID() {
