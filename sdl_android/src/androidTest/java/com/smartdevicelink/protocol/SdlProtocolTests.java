7fd955f658bf50d729e0de83f8f08cdfbc8fda2c
BrettyWhite <geekman3454@protonmail.com>
2019-02-27 16:17:29 -0500

    fix failing tests

--- sdl_android/src/androidTest/java/com/smartdevicelink/protocol/SdlProtocolTests.java
+++ sdl_android/src/androidTest/java/com/smartdevicelink/protocol/SdlProtocolTests.java
@@ -83,6 +83,8 @@ public class SdlProtocolTests  extends AndroidTestCase2 {
         public void setAcceptedVideoParams(VideoStreamingParameters acceptedVideoParams) {}
         @Override
         public void stopStream(SessionType serviceType) {}
+        @Override
+        public void onAuthTokenReceived(String token){}
     };
 
     DidReceiveListener onProtocolMessageReceivedListener = new DidReceiveListener();
--- sdl_android/src/androidTest/java/com/smartdevicelink/test/streaming/MockInterfaceBroker.java
+++ sdl_android/src/androidTest/java/com/smartdevicelink/test/streaming/MockInterfaceBroker.java
@@ -68,4 +68,6 @@ public class MockInterfaceBroker implements ISdlConnectionListener {
 			byte sessionID) {
 
 	}
+	@Override
+	public void onAuthTokenReceived(String token, byte bytes){}
 }
