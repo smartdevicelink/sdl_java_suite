e9e68b63ead876b1cbc3883abe10db239f262a7f
BrettyWhite <geekman3454@protonmail.com>
2019-03-13 13:59:28 -0400

    use AndroidTools

--- android/sdl_android/src/main/java/com/smartdevicelink/util/HttpUtils.java
+++ android/sdl_android/src/main/java/com/smartdevicelink/util/HttpUtils.java
@@ -1,23 +1,17 @@
 package com.smartdevicelink.util;
 
-import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.net.URL;
-import java.net.URLConnection;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 
+/**
+ * @see AndroidTools
+ */
 @Deprecated
 public class HttpUtils{
 
     public static Bitmap downloadImage(String urlStr) throws IOException{
-        URL url = new URL(urlStr);
-        URLConnection connection = url.openConnection();
-        BufferedInputStream bis = new BufferedInputStream(connection.getInputStream());
-        Bitmap result = BitmapFactory.decodeStream(bis);
-        bis.close();
-        return result;
+		return AndroidTools.downloadImage(urlStr);
     }
 
 }
