dd45f46e09f3405a25ba8325d2060d0832b5d323
Bilal Alsharifi <bilal.alsharifi@gmail.com>
2019-03-19 18:31:06 -0400

    Update version name to RC1-4.8.0

--- javaSE/src/main/java/com/smartdevicelink/BuildConfig.java
+++ javaSE/src/main/java/com/smartdevicelink/BuildConfig.java
@@ -1,5 +1,5 @@
 package com.smartdevicelink;
 
 public final class BuildConfig {
-    public static final String VERSION_NAME = "4.7.2";
+    public static final String VERSION_NAME = "RC1-4.8.0";
 }
