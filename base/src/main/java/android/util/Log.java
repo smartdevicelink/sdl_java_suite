619774dac748929d0a58732c62b81e585f7369c7
Joey Grover <joeygrover@gmail.com>
2019-03-29 16:27:36 -0400

    Fix potential NPE in Log.java

--- base/src/main/java/android/util/Log.java
+++ base/src/main/java/android/util/Log.java
@@ -51,11 +51,19 @@ public class Log {
 
     }
     public static int e(String tag, String message, Exception e){
-        System.out.print("\r\nERROR: " + tag+ " - " + message + " - " + e.getMessage());
+        if(e != null){
+            System.out.print("\r\nERROR: " + tag+ " - " + message + " - " + e.getMessage());
+        }else{
+            System.out.print("\r\nERROR: " + tag+ " - " + message);
+        }
         return 10;
     }
     public static int e(String tag, String message, Throwable t){
-        System.out.print("\r\nERROR: " + tag+ " - " + message + " - " + t.getMessage());
+        if(t != null){
+            System.out.print("\r\nERROR: " + tag+ " - " + message + " - " + t.getMessage());
+        }else{
+            System.out.print("\r\nERROR: " + tag+ " - " + message);
+        }
         return 10;
     }
 
