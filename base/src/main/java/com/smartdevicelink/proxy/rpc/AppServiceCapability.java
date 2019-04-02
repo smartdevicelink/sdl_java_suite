4fc9987d49f4bd8090186d1fd36a20903d7c2069
Joey Grover <joeygrover@gmail.com>
2019-04-02 17:49:04 -0400

    SCM listens for notification, ASCaps updated

--- base/src/main/java/com/smartdevicelink/proxy/rpc/AppServiceCapability.java
+++ base/src/main/java/com/smartdevicelink/proxy/rpc/AppServiceCapability.java
@@ -97,4 +97,32 @@ public class AppServiceCapability extends RPCStruct {
 		return (AppServiceRecord) getObject(AppServiceRecord.class, KEY_UPDATED_APP_SERVICE_RECORD);
 	}
 
+	/**
+	 * Helper method to compare an AppServiceCapability to this instance.
+	 * @param capability the AppServiceCapability to compare to this one
+	 * @return if both AppServiceCapability objects refer to the same service
+	 */
+	public boolean matchesAppService(AppServiceCapability capability){
+		if(capability != null){
+			AppServiceRecord appServiceRecord = getUpdatedAppServiceRecord();
+			AppServiceRecord otherASR = capability.getUpdatedAppServiceRecord();
+
+			if(appServiceRecord != null && otherASR != null) {
+				//Check service IDs, if they are the same we can assume these are the same
+				if (appServiceRecord.getServiceID() != null && appServiceRecord.getServiceID().equalsIgnoreCase(otherASR.getServiceID())) {
+					return true;
+				}else{
+					AppServiceManifest manifest = appServiceRecord.getServiceManifest();
+					AppServiceManifest otherManifest = otherASR.getServiceManifest();
+					if(manifest != null && otherManifest != null){
+						//Check the service names, if they are the same it can be assumed they are the same service
+						return (manifest.getServiceName() != null && manifest.getServiceName().equalsIgnoreCase(otherManifest.getServiceName()));
+					}
+				}
+			}
+		}
+		// If it got to this point it was not the same
+		return false;
+	}
+
 }
