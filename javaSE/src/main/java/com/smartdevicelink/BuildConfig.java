c85a78594ac8c22b44a5167227a4f3055a0e7714
Joey Grover <joeygrover@gmail.com>
2019-04-02 13:58:05 -0400

    Update gradle to handle TravisCI building

--- javaSE/src/main/java/com/smartdevicelink/BuildConfig.java
+++ javaSE/src/main/java/com/smartdevicelink/BuildConfig.java
@@ -30,7 +30,7 @@
 */
 package com.smartdevicelink;
 
-// THIS FILE IS AUTO GENERATED, DO NOT MODY!!
+// THIS FILE IS AUTO GENERATED, DO NOT MODIFY!!
 public final class BuildConfig {
     public static final String VERSION_NAME = "RC2-4.8.0";
 }
