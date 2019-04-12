382b344d13449a974613de5d014f68858732274a
Bilal Alsharifi <bilal.alsharifi@gmail.com>
2019-04-12 11:40:58 -0400

    Add null check in sendRPCMessagePrivate

--- android/sdl_android/src/main/java/com/smartdevicelink/proxy/SdlProxyBase.java
+++ android/sdl_android/src/main/java/com/smartdevicelink/proxy/SdlProxyBase.java
@@ -2076,18 +2076,20 @@ public abstract class SdlProxyBase<proxyListenerType extends IProxyListenerBase>
 
 					ButtonName buttonName = (ButtonName) request.getObject(ButtonName.class, SubscribeButton.KEY_BUTTON_NAME);
 
-					if (rpcSpecVersion != null && rpcSpecVersion.getMajor() < 5) {
+					if (rpcSpecVersion != null) {
+						if (rpcSpecVersion.getMajor() < 5) {
 
-						if (ButtonName.PLAY_PAUSE.equals(buttonName)) {
-							request.setParameters(SubscribeButton.KEY_BUTTON_NAME, ButtonName.OK);
-						}
-					} else { //Newer than version 5.0.0
-						if (ButtonName.OK.equals(buttonName)) {
-							RPCRequest request2 = new RPCRequest(request);
-							request2.setParameters(SubscribeButton.KEY_BUTTON_NAME, ButtonName.PLAY_PAUSE);
-							request2.setOnRPCResponseListener(request.getOnRPCResponseListener());
-							sendRPCMessagePrivate(request2);
-							return;
+							if (ButtonName.PLAY_PAUSE.equals(buttonName)) {
+								request.setParameters(SubscribeButton.KEY_BUTTON_NAME, ButtonName.OK);
+							}
+						} else { //Newer than version 5.0.0
+							if (ButtonName.OK.equals(buttonName)) {
+								RPCRequest request2 = new RPCRequest(request);
+								request2.setParameters(SubscribeButton.KEY_BUTTON_NAME, ButtonName.PLAY_PAUSE);
+								request2.setOnRPCResponseListener(request.getOnRPCResponseListener());
+								sendRPCMessagePrivate(request2);
+								return;
+							}
 						}
 					}
 				}
