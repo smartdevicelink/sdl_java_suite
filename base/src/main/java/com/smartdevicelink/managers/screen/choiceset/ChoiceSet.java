65327947963f5e11ca9c620d9048e88e52006e04
BrettyWhite <geekman3454@protonmail.com>
2019-05-29 14:36:17 -0400

    choiceset done, need to fix tests

--- base/src/main/java/com/smartdevicelink/managers/screen/choiceset/ChoiceSet.java
+++ base/src/main/java/com/smartdevicelink/managers/screen/choiceset/ChoiceSet.java
@@ -32,10 +32,17 @@
 
 package com.smartdevicelink.managers.screen.choiceset;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.smartdevicelink.exception.SdlException;
+import com.smartdevicelink.exception.SdlExceptionCause;
 import com.smartdevicelink.proxy.TTSChunkFactory;
 import com.smartdevicelink.proxy.rpc.TTSChunk;
 import com.smartdevicelink.proxy.rpc.VrHelpItem;
+import com.smartdevicelink.util.DebugTool;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ChoiceSet {
@@ -51,7 +58,7 @@ public class ChoiceSet {
     private Integer defaultTimeout = 10;
     private ChoiceSetLayout defaultLayout = ChoiceSetLayout.CHOICE_SET_LAYOUT_LIST;
 
-    public ChoiceSet(String title, ChoiceSetSelectionListener listener, List<ChoiceCell> choices) {
+    public ChoiceSet(@NonNull String title, @Nullable ChoiceSetSelectionListener listener, @NonNull List<ChoiceCell> choices) {
         setTitle(title);
         setChoiceSetSelectionListener(listener);
         setChoices(choices);
@@ -59,16 +66,20 @@ public class ChoiceSet {
         // defaults
         setLayout(defaultLayout);
         setTimeout(defaultTimeout);
+
+        // things to do
+        checkChoiceSetParameters();
+        setUpChoiceSet(null);
     }
 
-    public ChoiceSet(String title, ChoiceSetSelectionListener listener, ChoiceSetLayout layout, Integer timeout, String initialPrompt, String timeoutPrompt, String helpPrompt, List<VrHelpItem> helpList, List<ChoiceCell> choices) {
+    public ChoiceSet(@NonNull String title, @Nullable ChoiceSetSelectionListener listener, @Nullable ChoiceSetLayout layout, @Nullable Integer timeout, @Nullable String initialPrompt, @Nullable String timeoutPrompt, @Nullable String helpPrompt, @Nullable List<VrHelpItem> helpList, @NonNull List<ChoiceCell> choices) {
         setTitle(title);
         setChoiceSetSelectionListener(listener);
         setLayout(layout);
         setTimeout(timeout);
         setChoices(choices);
-        setVrHelpList(helpList);
 
+        // Help the dev by creating TTS chunks for them
         if (initialPrompt != null){
             setInitialPrompt(TTSChunkFactory.createSimpleTTSChunks(initialPrompt));
         }
@@ -78,19 +89,27 @@ public class ChoiceSet {
         }
 
         if (helpPrompt != null){
-            setTimeoutPrompt(TTSChunkFactory.createSimpleTTSChunks(helpPrompt));
+            setHelpPrompt(TTSChunkFactory.createSimpleTTSChunks(helpPrompt));
         }
+
+        // things to do
+        checkChoiceSetParameters();
+        setUpChoiceSet(helpList);
     }
 
-    public ChoiceSet(String title, ChoiceSetSelectionListener listener, List<TTSChunk> initialPrompt, List<TTSChunk> timeoutPrompt, List<TTSChunk> helpPrompt, List<ChoiceCell> choices) {
+    public ChoiceSet(@NonNull String title, @Nullable ChoiceSetSelectionListener listener, @Nullable ChoiceSetLayout layout, @Nullable Integer timeout, @Nullable List<TTSChunk> initialPrompt, @Nullable List<TTSChunk> timeoutPrompt, @Nullable List<TTSChunk> helpPrompt, @Nullable List<VrHelpItem> helpList, @NonNull List<ChoiceCell> choices) {
         setTitle(title);
         setChoiceSetSelectionListener(listener);
         setInitialPrompt(initialPrompt);
         setTimeoutPrompt(timeoutPrompt);
         setHelpPrompt(helpPrompt);
         setChoices(choices);
-        setTimeout(defaultTimeout);
-        setLayout(defaultLayout);
+        setTimeout(timeout);
+        setLayout(layout);
+
+        // things to do
+        checkChoiceSetParameters();
+        setUpChoiceSet(helpList);
     }
 
     public String getTitle() {
@@ -175,7 +194,77 @@ public class ChoiceSet {
 
     // HELPERS
 
-    public void checkChoiceSetParameters(){
+    private void checkChoiceSetParameters(){
+        if (DebugTool.isDebugEnabled()) {
+            if (getTitle() != null) {
+                if (getTitle().length() == 0 || getTitle().length() > 500) {
+                    DebugTool.logWarning("Attempted to create a choice set with a title of " + getTitle().length() + " length. Only 500 characters are supported.");
+                }
+            }
+            if (getTimeout() != null) {
+                if (getTimeout() < 5 || getTimeout() > 100) {
+                    DebugTool.logWarning("Attempted to create a choice set with a " + getTimeout() + " second timeout; Only 5 - 100 seconds is valid");
+                }
+            }
+            if (getChoices() != null) {
+                if (getChoices().size() == 0 || getChoices().size() > 100) {
+                    DebugTool.logWarning("Attempted to create a choice set with "+getChoices().size()+" choices; Only 1 - 100 choices are valid");
+                }
+            }
+        }
+    }
+
+    private void setUpChoiceSet(List<VrHelpItem> helpItems){
+
+        if (getChoices() == null){
+            try {
+                throw new SdlException("Cannot initiate a choice set with no choices", SdlExceptionCause.INVALID_ARGUMENT);
+            } catch (SdlException e) {
+                e.printStackTrace();
+            }
+        }
+
+        HashSet<String> choiceTextSet = new HashSet<>();
+        HashSet<String> uniqueVoiceCommands = new HashSet<>();
+        int allVoiceCommandsCount = 0;
+        int choiceCellWithVoiceCommandCount = 0;
+
+        for (ChoiceCell cell : getChoices()){
+
+            choiceTextSet.add(cell.getText());
+
+            if (cell.getVoiceCommands() == null){
+                continue;
+            }
+
+            uniqueVoiceCommands.addAll(cell.getVoiceCommands());
+            choiceCellWithVoiceCommandCount += 1;
+            allVoiceCommandsCount += cell.getVoiceCommands().size();
+        }
+
+        if (choiceTextSet.size() < choices.size()){
+            DebugTool.logError("Attempted to create a choice set with duplicate cell text. Cell text must be unique. The choice set will not be set.");
+            return;
+        }
+
+        // All or none of the choices MUST have VR Commands
+        if (choiceCellWithVoiceCommandCount > 0 && choiceCellWithVoiceCommandCount < choices.size()){
+            DebugTool.logError("If using voice recognition commands, all of the choice set cells must have unique VR commands. There are "+uniqueVoiceCommands.size()+" cells with unique voice commands and "+allVoiceCommandsCount+" total cells. The choice set will not be set.");
+            return;
+        }
+
+        // All VR Commands MUST be unique
+        if (uniqueVoiceCommands.size() < allVoiceCommandsCount){
+            DebugTool.logError("If using voice recognition commands, all VR commands must be unique. There are "+uniqueVoiceCommands.size()+" unique VR commands and "+allVoiceCommandsCount+" VR commands. The choice set will not be set.");
+            return;
+        }
+
+        if (helpItems != null) {
+            for (int i = 0; i < helpItems.size(); i++) {
+                helpItems.get(i).setPosition(i+1);
+            }
+            setVrHelpList(helpItems);
+        }
 
     }
 }
