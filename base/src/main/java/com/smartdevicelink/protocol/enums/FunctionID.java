7fe807f4421053f6e62ea6694dc842990f7f8407
BrettyWhite <geekman3454@protonmail.com>
2019-07-12 13:40:51 -0400

    add in RPCs, still need tests

--- base/src/main/java/com/smartdevicelink/protocol/enums/FunctionID.java
+++ base/src/main/java/com/smartdevicelink/protocol/enums/FunctionID.java
@@ -100,6 +100,7 @@ public enum FunctionID{
     GET_APP_SERVICE_DATA(53, "GetAppServiceData"),
     GET_FILE(54, "GetFile"),
     PERFORM_APP_SERVICES_INTERACTION(55, "PerformAppServiceInteraction"),
+    UNPUBLISH_APP_SERVICE(56, "UnpublishAppService"),
     CLOSE_APPLICATION(58, "CloseApplication"),
 
     // NOTIFICATIONS
