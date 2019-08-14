eb32ae3570f9dda2271a31b9e429990008380906
NicoleYarroch <nicole@livio.io>
2019-08-14 12:01:34 -0400

    Fixed spacing

--- android/sdl_android/src/androidTest/java/com/smartdevicelink/managers/screen/choiceset/ChoiceSetManagerTests.java
+++ android/sdl_android/src/androidTest/java/com/smartdevicelink/managers/screen/choiceset/ChoiceSetManagerTests.java
@@ -62,6 +62,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class ChoiceSetManagerTests extends AndroidTestCase2 {
+	
 	private ChoiceSetManager csm;
 
 	@Override
