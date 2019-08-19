1935855642c283b910ed7f6a795fbb42400effe5
Mauricio <mjuare44@ford.com>
2019-08-19 13:36:34 +0200

    fix tabulation and position

--- android/sdl_android/src/androidTest/java/com/smartdevicelink/test/rpc/datatypes/DisplayCapabilityTests.java
+++ android/sdl_android/src/androidTest/java/com/smartdevicelink/test/rpc/datatypes/DisplayCapabilityTests.java
@@ -7,25 +7,28 @@ import com.smartdevicelink.proxy.rpc.WindowTypeCapabilities;
 import com.smartdevicelink.test.JsonUtils;
 import com.smartdevicelink.test.Test;
 import com.smartdevicelink.test.Validator;
+
 import junit.framework.TestCase;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
 
 /**
- * This is a unit test class for the SmartDeviceLink library project class : 
- * {@link com.smartdevicelink.rpc.DisplayCapability}
+ * This is a unit test class for the SmartDeviceLink library project class :
+ * {@link com.smartdevicelink.proxy.rpc.DisplayCapability}
  */
-public class DisplayCapabilityTests extends TestCase{
-	
+public class DisplayCapabilityTests extends TestCase {
+
     private DisplayCapability msg;
 
     @Override
-    public void setUp(){    	
-    	msg = new DisplayCapability();
+    public void setUp() {
+        msg = new DisplayCapability();
 
         msg.setDisplayName(Test.GENERAL_STRING);
         msg.setWindowTypeSupported(Test.GENERAL_WINDOW_TYPE_CAPABILITIES_LIST);
@@ -35,25 +38,25 @@ public class DisplayCapabilityTests extends TestCase{
     /**
      * Tests the expected values of the RPC message.
      */
-    public void testRpcValues () {
-    	// Test Values
+    public void testRpcValues() {
+        // Test Values
         String displayName = msg.getDisplayName();
         List<WindowTypeCapabilities> windowTypeSupported = msg.getWindowTypeSupported();
         List<WindowCapability> windowCapabilities = msg.getWindowCapabilities();
 
         // Valid Tests
         assertEquals(Test.MATCH, Test.GENERAL_STRING, displayName);
-		assertEquals(Test.MATCH, Test.GENERAL_WINDOW_TYPE_CAPABILITIES_LIST.size(), windowTypeSupported.size());
+        assertEquals(Test.MATCH, Test.GENERAL_WINDOW_TYPE_CAPABILITIES_LIST.size(), windowTypeSupported.size());
         assertEquals(Test.MATCH, Test.GENERAL_WINDOW_CAPABILITY_LIST.size(), windowCapabilities.size());
-		
-        for(int i = 0; i < Test.GENERAL_WINDOW_TYPE_CAPABILITIES_LIST.size(); i++){
+
+        for (int i = 0; i < Test.GENERAL_WINDOW_TYPE_CAPABILITIES_LIST.size(); i++) {
             assertTrue(Test.TRUE, Validator.validateWindowTypeCapabilities(Test.GENERAL_WINDOW_TYPE_CAPABILITIES_LIST.get(i), windowTypeSupported.get(i)));
         }
 
-        for(int i = 0; i < Test.GENERAL_WINDOW_CAPABILITY_LIST.size(); i++){
+        for (int i = 0; i < Test.GENERAL_WINDOW_CAPABILITY_LIST.size(); i++) {
             assertTrue(Test.TRUE, Validator.validateWindowCapability(Test.GENERAL_WINDOW_CAPABILITY_LIST.get(i), windowCapabilities.get(i)));
         }
-        
+
         // Invalid/Null Tests
         DisplayCapability msg = new DisplayCapability();
         assertNotNull(Test.NOT_NULL, msg);
@@ -62,47 +65,47 @@ public class DisplayCapabilityTests extends TestCase{
         assertNull(Test.NULL, msg.getWindowTypeSupported());
         assertNull(Test.NULL, msg.getWindowCapabilities());
     }
-    
-    public void testJson(){
+
+    public void testJson() {
         JSONObject reference = new JSONObject();
 
-        try{
+        try {
             reference.put(DisplayCapability.KEY_DISPLAY_NAME, Test.GENERAL_STRING);
             reference.put(DisplayCapability.KEY_WINDOW_TYPE_SUPPORTED, Test.JSON_WINDOW_TYPE_CAPABILITIES_LIST);
             reference.put(DisplayCapability.KEY_WINDOW_CAPABILITIES, Test.JSON_WINDOW_CAPABILITIES);
 
             JSONObject underTest = msg.serializeJSON();
             assertEquals(Test.MATCH, reference.length(), underTest.length());
-            
+
             Iterator<?> iterator = reference.keys();
-            while(iterator.hasNext()){
+            while (iterator.hasNext()) {
                 String key = (String) iterator.next();
-                if(key.equals(DisplayCapability.KEY_WINDOW_TYPE_SUPPORTED)){
+                if (key.equals(DisplayCapability.KEY_WINDOW_TYPE_SUPPORTED)) {
                     JSONArray referenceArray = JsonUtils.readJsonArrayFromJsonObject(reference, key);
                     JSONArray underTestArray = JsonUtils.readJsonArrayFromJsonObject(underTest, key);
                     assertEquals(Test.MATCH, referenceArray.length(), underTestArray.length());
 
-                    for(int i = 0; i < referenceArray.length(); i++){
-                    	Hashtable<String, Object> hashReference = JsonRPCMarshaller.deserializeJSONObject(referenceArray.getJSONObject(i));
-                    	Hashtable<String, Object> hashTest= JsonRPCMarshaller.deserializeJSONObject(underTestArray.getJSONObject(i));
-                    	assertTrue(Test.TRUE, Validator.validateWindowTypeCapabilities(new WindowTypeCapabilities(hashReference), new WindowTypeCapabilities(hashTest)));
+                    for (int i = 0; i < referenceArray.length(); i++) {
+                        Hashtable<String, Object> hashReference = JsonRPCMarshaller.deserializeJSONObject(referenceArray.getJSONObject(i));
+                        Hashtable<String, Object> hashTest = JsonRPCMarshaller.deserializeJSONObject(underTestArray.getJSONObject(i));
+                        assertTrue(Test.TRUE, Validator.validateWindowTypeCapabilities(new WindowTypeCapabilities(hashReference), new WindowTypeCapabilities(hashTest)));
                     }
-                } else if(key.equals(DisplayCapability.KEY_WINDOW_CAPABILITIES)){
-                	JSONArray referenceArray = JsonUtils.readJsonArrayFromJsonObject(reference, key);
+                } else if (key.equals(DisplayCapability.KEY_WINDOW_CAPABILITIES)) {
+                    JSONArray referenceArray = JsonUtils.readJsonArrayFromJsonObject(reference, key);
                     JSONArray underTestArray = JsonUtils.readJsonArrayFromJsonObject(underTest, key);
                     assertEquals(Test.MATCH, referenceArray.length(), underTestArray.length());
 
-                    for(int i = 0; i < referenceArray.length(); i++){
-                    	Hashtable<String, Object> hashReference = JsonRPCMarshaller.deserializeJSONObject(referenceArray.getJSONObject(i));
-                    	Hashtable<String, Object> hashTest= JsonRPCMarshaller.deserializeJSONObject(underTestArray.getJSONObject(i));
-                    	assertTrue(Test.TRUE, Validator.validateWindowCapability(new WindowCapability(hashReference), new WindowCapability(hashTest)));
+                    for (int i = 0; i < referenceArray.length(); i++) {
+                        Hashtable<String, Object> hashReference = JsonRPCMarshaller.deserializeJSONObject(referenceArray.getJSONObject(i));
+                        Hashtable<String, Object> hashTest = JsonRPCMarshaller.deserializeJSONObject(underTestArray.getJSONObject(i));
+                        assertTrue(Test.TRUE, Validator.validateWindowCapability(new WindowCapability(hashReference), new WindowCapability(hashTest)));
                     }
-                } else{
+                } else {
                     assertEquals(Test.MATCH, JsonUtils.readObjectFromJsonObject(reference, key), JsonUtils.readObjectFromJsonObject(underTest, key));
                 }
             }
-        }catch(JSONException e){
-        	fail(Test.JSON_FAIL);
+        } catch (JSONException e) {
+            fail(Test.JSON_FAIL);
         }
     }
 }
