3949c9bd0f07ca92f33b49210b49b16409a96e21
NicoleYarroch <nicole@livio.io>
2019-08-21 08:37:46 -0400

    Added constructor to Cancel Interaction Response

--- base/src/main/java/com/smartdevicelink/proxy/rpc/CancelInteractionResponse.java
+++ base/src/main/java/com/smartdevicelink/proxy/rpc/CancelInteractionResponse.java
@@ -34,8 +34,11 @@
 
 package com.smartdevicelink.proxy.rpc;
 
+import android.support.annotation.NonNull;
+
 import com.smartdevicelink.protocol.enums.FunctionID;
 import com.smartdevicelink.proxy.RPCResponse;
+import com.smartdevicelink.proxy.rpc.enums.Result;
 
 import java.util.Hashtable;
 
@@ -55,5 +58,16 @@ public class CancelInteractionResponse extends RPCResponse {
      */
     public CancelInteractionResponse(Hashtable<String, Object> hash) {
         super(hash);
+    }
+
+    /**
+     * Constructs a new PublishAppServiceResponse object
+     * @param success whether the request is successfully processed
+     * @param resultCode whether the request is successfully processed
+     */
+    public CancelInteractionResponse(@NonNull Boolean success, @NonNull Result resultCode) {
+        this();
+        setSuccess(success);
+        setResultCode(resultCode);
     }
 }
