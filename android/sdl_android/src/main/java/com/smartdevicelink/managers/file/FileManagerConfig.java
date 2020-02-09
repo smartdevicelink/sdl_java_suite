dddedddc04b8d4fd741030467d124ee0fb3a2d1d
Julian Kast <julian@livio.com>
2020-02-09 16:52:44 -0500

    Added comments and removed Extra space

--- android/sdl_android/src/main/java/com/smartdevicelink/managers/file/FileManagerConfig.java
+++ android/sdl_android/src/main/java/com/smartdevicelink/managers/file/FileManagerConfig.java
@@ -1,5 +1,15 @@
 package com.smartdevicelink.managers.file;
 
+/**
+ * <strong>FileManagerConfig</strong> <br>
+ *
+ * This is set during SdlManager instantiation. <br>
+ *
+ * <li> artWorkRetryCount - if not set, will be set to 1 to allow for retry upload attempts for SDLArtWork</li>
+ *
+ * <li> fileRetryCount - if not set, will be set to 1 to allow for retry upload attempts for SDLFiles</li>
+ */
+
 public class FileManagerConfig {
 
     private int artworkRetryCount, fileRetryCount;
@@ -12,17 +22,11 @@ public class FileManagerConfig {
 
     public void setArtworkRetryCount(int artworkRetryCount) { this.artworkRetryCount = artworkRetryCount; }
 
-
-    public int getArtworkRetryCount() {
-        return artworkRetryCount;
-    }
+    public int getArtworkRetryCount() { return artworkRetryCount; }
 
     public void setFileRetryCount(int fileRetryCount) { this.fileRetryCount = fileRetryCount; }
 
-
-    public int getFileRetryCount() {
-        return fileRetryCount;
-    }
+    public int getFileRetryCount() { return fileRetryCount; }
 
 }
 
