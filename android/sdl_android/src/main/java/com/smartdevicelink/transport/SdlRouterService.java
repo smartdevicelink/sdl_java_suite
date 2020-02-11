024cebb2bd45ebcb5cf818e3c2ac5fa77c7b5a7f
Joey Grover <joeygrover@gmail.com>
2020-02-11 12:13:26 -0500

    Increment router service version

--- android/sdl_android/src/main/java/com/smartdevicelink/transport/SdlRouterService.java
+++ android/sdl_android/src/main/java/com/smartdevicelink/transport/SdlRouterService.java
@@ -139,7 +139,7 @@ public class SdlRouterService extends Service{
 	/**
 	 * <b> NOTE: DO NOT MODIFY THIS UNLESS YOU KNOW WHAT YOU'RE DOING.</b>
 	 */
-	protected static final int ROUTER_SERVICE_VERSION_NUMBER = 10;
+	protected static final int ROUTER_SERVICE_VERSION_NUMBER = 11;
 
 	private static final String ROUTER_SERVICE_PROCESS = "com.smartdevicelink.router";
 	
