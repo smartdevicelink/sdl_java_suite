0077a95e06c47b35d863e01bd933cfc182d306b4
Julian Kast <julian@livio.com>
2020-02-20 16:04:34 -0500

    Removed unused import

--- base/src/main/java/com/smartdevicelink/managers/file/BaseFileManager.java
+++ base/src/main/java/com/smartdevicelink/managers/file/BaseFileManager.java
@@ -60,7 +60,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 
