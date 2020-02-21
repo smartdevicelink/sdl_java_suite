746c75b95285f42ab95dcaa34399a697d2d6b409
Julian Kast <julian@livio.com>
2020-02-21 09:55:04 -0500

    Added Override equals to SdlFile in javaSE

--- javaSE/src/main/java/com/smartdevicelink/managers/file/filetypes/SdlFile.java
+++ javaSE/src/main/java/com/smartdevicelink/managers/file/filetypes/SdlFile.java
@@ -185,4 +185,42 @@ public class SdlFile{
     public boolean isStaticIcon() {
         return isStaticIcon;
     }
+
+    /**
+     * Used to compile hashcode for SdlFile for use to compare in equals method
+     *
+     * @return Custom hashcode of SdlFile variables
+     */
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result += ((getName() == null) ? 0 : Integer.rotateLeft(getName().hashCode(), 1));
+        result += ((getFilePath() == null) ? 0 : Integer.rotateLeft(getFilePath().hashCode(), 2));
+        result += ((getFileData() == null) ? 0 : Integer.rotateLeft(getFileData().hashCode(), 3));
+        result += ((getType() == null) ? 0 : Integer.rotateLeft(getType().hashCode(), 4));
+        return result;
+    }
+
+    /**
+     * Uses our custom hashCode for SdlFile objects
+     *
+     * @param o - The object to compare
+     * @return boolean of whether the objects are the same or not
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        // if this is the same memory address, it's the same
+        if (this == o) return true;
+        // if this is not an instance of this class, not the same
+        if (!(o instanceof SdlFile)) return false;
+        // Cast o to SdlFile
+        SdlFile sdlFile = (SdlFile) o;
+        // if isStaticIcon and isPersistent is not the same for each object, they are not the same
+        if (!(this.isStaticIcon == sdlFile.isStaticIcon && this.isPersistent() == sdlFile.isPersistent())) {
+            return false;
+        }
+        // return comparison
+        return hashCode() == o.hashCode();
+    }
 }
