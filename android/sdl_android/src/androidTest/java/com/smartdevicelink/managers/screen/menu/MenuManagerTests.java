f5b798cad49d91bc43d303520c6d09e4ec7de580
RHenigan <heniganr1@gmail.com>
2020-05-28 13:26:20 -0400

    Update unit tests for voice commands lists

--- android/sdl_android/src/androidTest/java/com/smartdevicelink/managers/screen/menu/MenuManagerTests.java
+++ android/sdl_android/src/androidTest/java/com/smartdevicelink/managers/screen/menu/MenuManagerTests.java
@@ -469,7 +469,26 @@ public class MenuManagerTests extends AndroidTestCase2 {
 	}
 
 	public void testSettingMenuWithEmptyVoiceCommands(){
+		// Make sure we can send an empty menu with no issues
+		// start fresh
+		menuManager.oldMenuCells = null;
+		menuManager.menuCells = null;
+		menuManager.inProgressUpdate = null;
+		menuManager.waitingUpdateMenuCells = null;
+		menuManager.waitingOnHMIUpdate = false;
 
+		MenuSelectionListener menuSelectionListener = mock(MenuSelectionListener.class);
+
+		MenuCell A = new MenuCell("A", null, Collections.EMPTY_LIST, menuSelectionListener);
+
+		menuManager.currentHMILevel = HMILevel.HMI_FULL;
+		List<MenuCell> newMenu = Collections.singletonList(A);
+
+		menuManager.setMenuCells(newMenu);
+		assertEquals(menuManager.menuCells.get(0).getVoiceCommands(), Collections.EMPTY_LIST);
+	}
+
+	public void testSettingMenuWithVoiceCommands(){
 		// Make sure we can send an empty menu with no issues
 		// start fresh
 		menuManager.oldMenuCells = null;
@@ -481,17 +500,15 @@ public class MenuManagerTests extends AndroidTestCase2 {
 
 		MenuSelectionListener menuSelectionListener = mock(MenuSelectionListener.class);
 
-		MenuCell A = new MenuCell("A", null, Collections.EMPTY_LIST, menuSelectionListener);
+		List<String> voiceCommandList = Collections.singletonList("Hello There");
+
+		MenuCell A = new MenuCell("A", null, voiceCommandList, menuSelectionListener);
 
 		menuManager.currentHMILevel = HMILevel.HMI_FULL;
-		// send new cells. They should set the old way
-		List<MenuCell> oldMenu = createDynamicMenu1();
 		List<MenuCell> newMenu = Collections.singletonList(A);
-		menuManager.setMenuCells(oldMenu);
-		assertEquals(menuManager.menuCells.size(), 4);
 
 		menuManager.setMenuCells(newMenu);
-		assertEquals(menuManager.menuCells.size(), 1);
+		assertEquals(menuManager.menuCells.get(0).getVoiceCommands(), voiceCommandList);
 	}
 
 
