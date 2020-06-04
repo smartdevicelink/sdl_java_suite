1ba0f78dc0ba752e117124e6da484e8d57ce943d
RHenigan <heniganr1@gmail.com>
2020-06-04 17:30:15 -0400

    Create a Maven Hello SDL JavaEE Project

---
+++ hello_sdl_java_ee/out/artifacts/hello_sdl_java_ee_war_exploded/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: rheniga1
+  Date: 6/4/20
+  Time: 5:11 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
--- hello_sdl_java_ee/src/main/java/com/smartdevicelink/Main.java
+++ hello_sdl_java_ee/src/main/java/com/smartdevicelink/Main.java
@@ -1,24 +1,34 @@
 package com.smartdevicelink;
 
+import com.smartdevicelink.transport.CustomTransport;
+import com.smartdevicelink.transport.CustomTransportConfig;
+
+import javax.ejb.Stateful;
+import javax.websocket.EndpointConfig;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
 /**
  * This is a sample of how to get Java EE Bean to work with an SDL application
  * The code can be uncommented out with the proper gradle dependencies added
  */
 
 
-//    @ServerEndpoint("/")
- //   @Stateful(name = "SDLSessionEJB")
-    public class Main {
+@ServerEndpoint("/")
+@Stateful(name = "SDLSessionEJB")
+public class Main {
 
-  /*      Session session;
+        Session session;
         static Thread thread = null, mainThread;
         static Object LOCK;
-
         static com.smartdevicelink.SdlService sdlService;
         CustomTransport websocket;
-
         @OnOpen
-        public void onOpen (Session session, EndpointConfig config) {
+        public void onOpen (final Session session, EndpointConfig config) {
             websocket = new CustomTransport("http://localhost") {
                 @Override
                 public void onWrite(byte[] bytes, int offset, int length) {
@@ -26,7 +36,6 @@ package com.smartdevicelink;
                         session.getBasicRemote().sendBinary(ByteBuffer.wrap(bytes));
                     }
                     catch (IOException e) {
-
                     }
                 }
             };
@@ -34,18 +43,13 @@ package com.smartdevicelink;
             sdlService = new com.smartdevicelink.SdlService(new CustomTransportConfig(websocket), sdlServiceCallback);
             sdlService.start();
         }
-
         @OnMessage
         public void onMessage (ByteBuffer message, Session session) {
             websocket.onByteBufferReceived(message);
         }
-
-
         static final com.smartdevicelink.SdlService.SdlServiceCallback sdlServiceCallback = new com.smartdevicelink.SdlService.SdlServiceCallback() {
             @Override
             public void onEnd() {
-
             }
         };
-        */
-    }
+}
