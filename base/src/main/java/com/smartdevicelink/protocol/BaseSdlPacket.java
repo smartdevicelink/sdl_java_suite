bb06b8de745c17dd472a10114815573a2ec52801
RHenigan <heniganr1@gmail.com>
2020-06-10 14:58:20 -0400

    Remove Parcelable implementation from base

--- base/src/main/java/com/smartdevicelink/protocol/BaseSdlPacket.java
+++ base/src/main/java/com/smartdevicelink/protocol/BaseSdlPacket.java
@@ -47,7 +47,7 @@ import java.util.HashMap;
  * Any other binder transactions must include an additional int flag into their bundle or the parsing
  * of this object will fail.
  */
-public class BaseSdlPacket implements Parcelable{
+public class BaseSdlPacket {
 
 	/**
 	 * This is the amount of bytes added to the bundle from the router service for a specific int
@@ -359,92 +359,6 @@ public class BaseSdlPacket implements Parcelable{
 	public void setMessagingVersion(int version){
 		this.messagingVersion = version;
 	}
-	
-	
-	
-	/* ***************************************************************************************************************************************************
-	 * ***********************************************************  Parceable Overrides  *****************************************************************
-	 *****************************************************************************************************************************************************/
-
-
-
-	//I think this is FIFO...right?
-	public BaseSdlPacket(Parcel p) {
-		this.version = p.readInt();
-		this.encryption = (p.readInt() == 0) ? false : true;
-		this.frameType = p.readInt();
-		this.serviceType = p.readInt();
-		this.frameInfo = p.readInt();
-		this.sessionId = p.readInt();
-		this.dataSize = p.readInt();
-		this.messageId = p.readInt();
-		if(p.readInt() == 1){ //We should have a payload attached
-			payload = new byte[dataSize];
-			p.readByteArray(payload);
-		}
-
-		this.priorityCoefficient = p.readInt();
-
-		if(p.dataAvail() > EXTRA_PARCEL_DATA_LENGTH) {	//See note on constant for why not 0
-			try {
-				messagingVersion = p.readInt();
-				if (messagingVersion >= 2) {
-					if (p.readInt() == 1) { //We should have a transport type attached
-						this.transportRecord = (TransportRecord) p.readParcelable(TransportRecord.class.getClassLoader());
-					}
-				}
-			}catch (RuntimeException e){
-				DebugTool.logError("Error creating packet from parcel", e);
-			}
-		}
-	}
-	
-	
-	@Override
-	public int describeContents() {
-		return 0;
-	}
-
-	@Override
-	public void writeToParcel(Parcel dest, int flags) {
-		
-		dest.writeInt(version);
-		dest.writeInt(encryption? 1 : 0);
-		dest.writeInt(frameType);
-		dest.writeInt(serviceType);
-		dest.writeInt(frameInfo);
-		dest.writeInt(sessionId);
-		dest.writeInt(dataSize);
-		dest.writeInt(messageId);
-		dest.writeInt(payload!=null? 1 : 0);
-		if(payload!=null){
-			dest.writeByteArray(payload);
-		}
-		dest.writeInt(priorityCoefficient);
-
-		///Additions after initial creation
-		if(messagingVersion > 1){
-			dest.writeInt(messagingVersion);
-
-			dest.writeInt(transportRecord!=null? 1 : 0);
-			if(transportRecord != null){
-				dest.writeParcelable(transportRecord,0);
-			}
-		}
-
-	}
-	
-	public static final Parcelable.Creator<BaseSdlPacket> CREATOR = new Parcelable.Creator<BaseSdlPacket>() {
-        public BaseSdlPacket createFromParcel(Parcel in) {
-     	   return new BaseSdlPacket(in);
-        }
-
-		@Override
-		public BaseSdlPacket[] newArray(int size) {
-			return new BaseSdlPacket[size];
-		}
-
-    };
 
 	public void putTag(String tag, Object data){
 		if(bsonPayload == null){
