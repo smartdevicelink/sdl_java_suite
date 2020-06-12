dcdbbbebf3a147ea35cbb9e5145072128ae1218f
RHenigan <heniganr1@gmail.com>
2020-06-12 16:20:04 -0400

    Comment our Main.java from hello_sdl_java_ee

--- hello_sdl_java_ee/src/com/smartdevicelink/Main.java
+++ hello_sdl_java_ee/src/com/smartdevicelink/Main.java
@@ -1,28 +1,16 @@
 package com.smartdevicelink;
 
-import com.smartdevicelink.transport.CustomTransport;
-import com.smartdevicelink.transport.CustomTransportConfig;
-
-import javax.ejb.Stateful;
-import javax.websocket.EndpointConfig;
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
-import javax.websocket.Session;
-import javax.websocket.server.ServerEndpoint;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-
 /**
  * This is a sample of how to get Java EE Bean to work with an SDL application
  * The code can be uncommented out with the proper gradle dependencies added
  */
 
 
-@ServerEndpoint("/")
-@Stateful(name = "SDLSessionEJB")
-public class Main {
+//    @ServerEndpoint("/")
+ //   @Stateful(name = "SDLSessionEJB")
+    public class Main {
 
-        Session session;
+  /*      Session session;
         static Thread thread = null, mainThread;
         static Object LOCK;
 
@@ -59,4 +47,5 @@ public class Main {
 
             }
         };
-}
+        */
+    }
