53b98f23c4bb147dfad13d9dc4fdd89a2e1f5fe5
RHenigan <heniganr1@gmail.com>
2020-06-26 10:03:19 -0400

    Update DebugTool to accept Tags

--- base/src/main/java/com/smartdevicelink/util/DebugTool.java
+++ base/src/main/java/com/smartdevicelink/util/DebugTool.java
@@ -50,7 +50,7 @@ import java.util.Vector;
 public class DebugTool {
 	
 
-	public static final String TAG = "SdlProxy";
+	public static final String TAG = "SDL";
 
 	private static boolean isErrorEnabled = false;
 	private static boolean isWarningEnabled = false;
@@ -84,71 +84,95 @@ public class DebugTool {
 		return string;
 	}
 
-	public static void logError(String msg) {
-		
+	public static void logError(String tag, String msg) {
 		Boolean wasWritten = false;
-		
+
 		msg = prependProxyVersionNumberToString(msg);
-		
+
 		wasWritten = logToSiphon(msg);
-		
+
 		if (isErrorEnabled && !wasWritten) {
-			NativeLogTool.logError(TAG, msg);
+			NativeLogTool.logError(tag, msg);
 		}
 	}
 
-	public static void logError(String msg, Throwable ex) {
+	@Deprecated
+	public static void logError(String msg) {
+		logError(TAG, msg);
+	}
+
+	public static void logError(String tag, String msg, Throwable ex) {
 		Boolean wasWritten = false;
-		
+
 		msg = prependProxyVersionNumberToString(msg);
-		
+
 		if (ex != null) {
 			wasWritten = logToSiphon(msg + " Exception String: " + ex.toString());
 		} else {
 			wasWritten = logToSiphon(msg);
 		}
-		
+
 		if (isErrorEnabled && !wasWritten) {
-			NativeLogTool.logError(TAG, msg, ex);
+			NativeLogTool.logError(tag, msg, ex);
 		}
 	}
-	
-	public static void logWarning(String msg) {
+
+	@Deprecated
+	public static void logError(String msg, Throwable ex) {
+		logError(TAG, msg, ex);
+	}
+
+	public static void logWarning(String tag, String msg) {
 		Boolean wasWritten = false;
-		
+
 		msg = prependProxyVersionNumberToString(msg);
-		
+
 		wasWritten = logToSiphon(msg);
-		
+
 		if (isWarningEnabled && !wasWritten) {
-			NativeLogTool.logWarning(TAG, msg);
+			NativeLogTool.logWarning(tag, msg);
 		}
 	}
 
-	public static void logInfo(String msg) {
+	@Deprecated
+	public static void logWarning(String msg) {
+		logWarning(TAG, msg);
+	}
+
+	public static void logInfo(String tag, String msg) {
 		Boolean wasWritten = false;
-		
+
 		msg = prependProxyVersionNumberToString(msg);
-		
+
 		wasWritten = logToSiphon(msg);
-		
+
 		if (isInfoEnabled && !wasWritten) {
-			NativeLogTool.logInfo(TAG, msg);
+			NativeLogTool.logInfo(tag, msg);
 		}
 	}
 
-	public static void logInfo(String msg, boolean bPrependVersion) {
+	@Deprecated
+	public static void logInfo(String msg) {
+		logInfo(TAG, msg);
+	}
+
+	public static void logInfo(String tag, String msg, Boolean bPrependVersion) {
 		Boolean wasWritten = false;
-		
+
 		if (bPrependVersion) msg = prependProxyVersionNumberToString(msg);
-		
+
 		wasWritten = logToSiphon(msg);
-		
+
 		if (isInfoEnabled && !wasWritten) {
-			NativeLogTool.logInfo(TAG, msg);
+			NativeLogTool.logInfo(tag, msg);
 		}
 	}
-	
+
+	@Deprecated
+	public static void logInfo(String msg, boolean bPrependVersion) {
+		logInfo(TAG, msg, bPrependVersion);
+	}
+
 	protected static Boolean logToSiphon(String msg) {
 		if (SiphonServer.getSiphonEnabledStatus()) {
 			// Initialize the SiphonServer, will be ignored if already initialized
