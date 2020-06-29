10d9bf489f950c9953ad286380806546356c929b
Julian Kast <julian@livio.com>
2020-06-29 13:01:01 -0400

    Fixed formatting

--- base/src/main/java/com/smartdevicelink/managers/screen/OnButtonListener.java
+++ base/src/main/java/com/smartdevicelink/managers/screen/OnButtonListener.java
@@ -4,29 +4,29 @@ import com.smartdevicelink.proxy.rpc.OnButtonEvent;
 import com.smartdevicelink.proxy.rpc.OnButtonPress;
 import com.smartdevicelink.proxy.rpc.enums.ButtonName;
 
+/**
+ * OnButtonListener is a listener used for notifying when events have happened with SubscribeButtons
+ */
+public interface OnButtonListener {
+
     /**
-     * OnButtonListener is a listener used for notifying when events have happened with SubscribeButtons
+     * Returns when a Subscribed button is pressed
+     * @param buttonName  - Name of Button
+     * @param buttonPress - OnButtonPress
      */
-    public interface OnButtonListener {
+    void onPress(ButtonName buttonName, OnButtonPress buttonPress);
 
-        /**
-         * Returns when a Subscribed button is pressed
-         * @param buttonName - Name of Button
-         * @param buttonPress - OnButtonPress
-         */
-        void onPress(ButtonName buttonName, OnButtonPress buttonPress);
+    /**
+     * Returns when a Subscribed button Event has occurred
+     * @param buttonName  - Name of Button
+     * @param buttonEvent - OnButtonEvent
+     */
+    void onEvent(ButtonName buttonName, OnButtonEvent buttonEvent);
 
-        /**
-         * Returns when a Subscribed button Event has occurred
-         * @param buttonName - Name of Button
-         * @param buttonEvent - OnButtonEvent
-         */
-        void onEvent(ButtonName buttonName, OnButtonEvent buttonEvent);
-
-        /**
-         * Returns when there is an error with subscribing to button
-         * @param info - Error info
-         */
-        void onError(String info);
-    }
+    /**
+     * Returns when there is an error with subscribing to button
+     * @param info - Error info
+     */
+    void onError(String info);
 }
+
