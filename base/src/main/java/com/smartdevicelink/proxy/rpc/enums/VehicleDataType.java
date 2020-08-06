f2d03434ad03cee0672c90cea68bcb19d0c4aaa3
Kostiantyn Boskin <kostyaboskin@gmail.com>
2020-08-06 16:11:14 +0300

    Update VehicleDataType.java
    [0257]
        Rollback VehicleDataType

--- base/src/main/java/com/smartdevicelink/proxy/rpc/enums/VehicleDataType.java
+++ base/src/main/java/com/smartdevicelink/proxy/rpc/enums/VehicleDataType.java
@@ -1,41 +1,3 @@
-/*
- * Copyright (c) 2017 - 2019, SmartDeviceLink Consortium, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following
- * disclaimer in the documentation and/or other materials provided with the
- * distribution.
- *
- * Neither the name of the SmartDeviceLink Consortium, Inc. nor the names of its
- * contributors may be used to endorse or promote products derived from this 
- * software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-package com.smartdevicelink.proxy.rpc.enums;
-
-/**
- * Defines the vehicle data types that can be published and subscribed to.
- * 
- */
-public enum VehicleDataType {
 	/**
 	 * Notifies GPSData may be subscribed
 	 */
@@ -171,7 +133,8 @@ public enum VehicleDataType {
     /**
      * @since SmartDeviceLink 7.0.0
      */
-    VEHICLEDATA_HANDSOFFSTEERING;
+    VEHICLEDATA_HANDSOFFSTEERING
+    ;
 
      /**
      * Convert String to VehicleDataType
@@ -186,4 +149,3 @@ public enum VehicleDataType {
             return null;
         }
     }
-}
