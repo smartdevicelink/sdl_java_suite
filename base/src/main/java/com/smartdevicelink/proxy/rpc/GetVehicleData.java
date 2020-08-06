3561e7ae729b1cac1a841b685a4e7b81aee4c535
Vladyslav Mustafin <vmustafin@luxoft.com>
2020-08-06 16:29:22 +0300

    Rollback other formatting

--- base/src/main/java/com/smartdevicelink/proxy/rpc/GetVehicleData.java
+++ base/src/main/java/com/smartdevicelink/proxy/rpc/GetVehicleData.java
@@ -233,6 +233,13 @@ import java.util.Hashtable;
  *				<td>Subscribable</td>
  * 			<td>SmartDeviceLink 5.1 </td>
  * 		</tr>
+ *      <tr>
+ *          <td>handsOffSteering</td>
+ *          <td>Boolean</td>
+ *          <td>To indicate whether driver hands are off the steering wheel</td>
+ *          <td>N</td>
+ *          <td>SmartDeviceLink 7.0.0</td>
+ *      </tr>
  * 		</table>
  *  
  *  
@@ -287,6 +294,7 @@ public class GetVehicleData extends RPCRequest {
 	public static final String KEY_TURN_SIGNAL = "turnSignal";
 	public static final String KEY_ELECTRONIC_PARK_BRAKE_STATUS = "electronicParkBrakeStatus";
     public static final String KEY_CLOUD_APP_VEHICLE_ID = "cloudAppVehicleID";
+    public static final String KEY_HANDS_OFF_STEERING = "handsOffSteering";
 
 	/**
 	 * Constructs a new GetVehicleData object
@@ -556,5 +564,25 @@ public class GetVehicleData extends RPCRequest {
      */
     public Boolean getOEMCustomVehicleData(String vehicleDataName){
         return getBoolean(vehicleDataName);
+    }
+
+    /**
+     * Sets the handsOffSteering.
+     *
+     * @param handsOffSteering To indicate whether driver hands are off the steering wheel
+     * @since SmartDeviceLink 7.0.0
+     */
+    public void setHandsOffSteering(Boolean handsOffSteering) {
+        setParameters(KEY_HANDS_OFF_STEERING, handsOffSteering);
+    }
+
+    /**
+     * Gets the handsOffSteering.
+     *
+     * @return Boolean To indicate whether driver hands are off the steering wheel
+     * @since SmartDeviceLink 7.0.0
+     */
+    public Boolean getHandsOffSteering() {
+        return getBoolean(KEY_HANDS_OFF_STEERING);
     }
 }
