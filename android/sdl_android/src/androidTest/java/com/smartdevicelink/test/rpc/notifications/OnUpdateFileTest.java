07035805658f5656239d46d3316a9f4d0e0d1f6b
RHenigan <heniganr1@gmail.com>
2020-08-14 11:42:40 -0400

    Add new test files for new notifications

---
+++ android/sdl_android/src/androidTest/java/com/smartdevicelink/test/rpc/notifications/OnUpdateFileTest.java
@@ -0,0 +1,73 @@
+package com.smartdevicelink.test.rpc.notifications;
+
+import com.smartdevicelink.protocol.enums.FunctionID;
+import com.smartdevicelink.proxy.RPCMessage;
+import com.smartdevicelink.proxy.rpc.OnUpdateFile;
+import com.smartdevicelink.test.BaseRpcTests;
+import com.smartdevicelink.test.TestValues;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+/**
+ * This is a unit test class for the SmartDeviceLink library project class :
+ * {@link com.smartdevicelink.rpc.OnUpdateFile}
+ */
+public class OnUpdateFileTest extends BaseRpcTests {
+    @Override
+    protected RPCMessage createMessage() {
+        OnUpdateFile msg = new OnUpdateFile();
+
+        msg.setFileName(TestValues.GENERAL_STRING);
+
+        return msg;
+    }
+
+    @Override
+    protected String getMessageType() {
+        return RPCMessage.KEY_NOTIFICATION;
+    }
+
+    @Override
+    protected String getCommandType() {
+        return FunctionID.ON_UPDATE_FILE.toString();
+    }
+
+    @Override
+    protected JSONObject getExpectedParameters(int sdlVersion) {
+        JSONObject result = new JSONObject();
+
+        try{
+            result.put(OnUpdateFile.KEY_FILE_NAME, TestValues.GENERAL_STRING);
+        }catch(JSONException e){
+            fail(TestValues.JSON_FAIL);
+        }
+
+        return result;
+    }
+
+    /**
+     * Tests the expected values of the RPC message.
+     */
+    @Test
+    public void testRpcValues () {
+        // Test Values
+        String fileName = ( (OnUpdateFile) msg).getFileName();
+
+        // Valid Tests
+        assertEquals(TestValues.MATCH, TestValues.GENERAL_STRING, fileName);
+
+        // Invalid/Null Tests
+        OnUpdateFile msg = new OnUpdateFile();
+        assertNotNull(TestValues.NOT_NULL, msg);
+        testNullBase(msg);
+
+        assertNull(TestValues.NULL, msg.getFileName());
+    }
+}
