045d0a856d7f4734dc0af6b87bb7b8a25d127b18
Kostiantyn Boskin <kostyaboskin@gmail.com>
2020-08-18 12:11:58 +0300

    Update base/src/main/java/com/smartdevicelink/proxy/rpc/GetVehicleDataResponse.java
    Co-authored-by: Robert Henigan <heniganr1@gmail.com>

--- base/src/main/java/com/smartdevicelink/proxy/rpc/GetVehicleDataResponse.java
+++ base/src/main/java/com/smartdevicelink/proxy/rpc/GetVehicleDataResponse.java
@@ -174,7 +174,7 @@ public class GetVehicleDataResponse extends RPCResponse {
      * Sets the prndl.
      *
      * @param prndl See PRNDL. This parameter is deprecated and it is now covered in `gearStatus`
-     * @since SmartDeviceLink 7.0.0
+     * @deprecated in SmartDeviceLink 7.0.0
      */
     @Deprecated
     public void setPrndl(PRNDL prndl) {
