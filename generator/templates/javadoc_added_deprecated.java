c7ff8ea8646193e2db3688c4fefb26dba34ce1f7
Bilal Alsharifi <bilal.alsharifi@gmail.com>
2020-08-20 17:16:33 -0400

    Refactor since & deprecated generation format

--- generator/templates/javadoc_added_deprecated.java
+++ generator/templates/javadoc_added_deprecated.java
@@ -2,12 +2,13 @@
 {%- if begin is defined %}
 {{spacing}}{{begin}}
 {%- endif %}
-{%- if deprecated is defined and deprecated is not none and since is defined and since is not none %}
+{%- if deprecated is defined and deprecated is not none %}
+{{spacing}} * @since in SmartDeviceLink {{history[0].since}}
 {{spacing}} * @deprecated in SmartDeviceLink {{since}}
-{%- elif deprecated is defined and deprecated is not none %}
-{{spacing}} * @deprecated
-{%- elif since is defined and since is not none %}
-{{spacing}} * @since SmartDeviceLink {{since}}
+{%- elif history is defined and history is not none %}
+{{spacing}} * @since in SmartDeviceLink {{history[0].since}}
+{%- else %}
+{{spacing}} * @since in SmartDeviceLink {{since}}
 {%- endif %}
 {%- if see is defined %}
 {{spacing}} * @see {{see}}
