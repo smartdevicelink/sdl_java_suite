05ba7c077dfa2b78f06610a117994fdea0ae9265
Julian Kast <julian@livio.com>
2020-09-02 10:32:44 -0400

    Remove line from deprecated method

--- android/sdl_android/src/main/java/com/smartdevicelink/managers/file/FileManager.java
+++ android/sdl_android/src/main/java/com/smartdevicelink/managers/file/FileManager.java
@@ -67,7 +67,6 @@ public class FileManager extends BaseFileManager {
 	private final WeakReference<Context> context;
 
 	@Deprecated
-	@RestrictTo(RestrictTo.Scope.LIBRARY)
 	public FileManager(ISdl internalInterface, Context context) {
 
 		// setup
