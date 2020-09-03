f14ec97d16f2bc4ce47630f947e61c63870a46f4
Bilal Alsharifi <599206+bilal-alsharifi@users.noreply.github.com>
2020-09-03 10:52:55 -0400

    Remove Deprecated APIs Part 2 (#1479)
    * Remove deprecated code in streaming package
    
    * Fix streaming unit tests
    
    * Remove deprecated IProtocolListener class
    
    * Remove unsed packetizer testing code
    
    * Remove deprecated IPutFileResponseListener

--- android/sdl_android/src/androidTest/java/com/smartdevicelink/test/streaming/MockPacketizer.java
+++ android/sdl_android/src/androidTest/java/com/smartdevicelink/test/streaming/MockPacketizer.java
@@ -2,10 +2,8 @@ package com.smartdevicelink.test.streaming;
 
 import com.smartdevicelink.SdlConnection.SdlSession;
 import com.smartdevicelink.protocol.enums.SessionType;
-import com.smartdevicelink.proxy.RPCRequest;
 import com.smartdevicelink.streaming.AbstractPacketizer;
 import com.smartdevicelink.streaming.IStreamListener;
-import com.smartdevicelink.util.Version;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,8 +14,6 @@ import java.io.InputStream;
  */
 public class MockPacketizer extends AbstractPacketizer {
 	public MockPacketizer (IStreamListener l, InputStream i, SessionType s, byte sid, SdlSession sdlsession) throws IOException { super (l, i, s, sid, sdlsession); }
-	public MockPacketizer (IStreamListener l, InputStream i, RPCRequest r, SessionType s, byte sid, Version protocolVersion,SdlSession sdlsession) throws IOException { super (l, i, r, s, sid, protocolVersion, sdlsession); }
-	public MockPacketizer (IStreamListener l, InputStream i, RPCRequest r, SessionType s, byte sid, byte w, SdlSession sdlsession) throws IOException { super (l, i, r, s, sid, w, sdlsession); }
 
 	@Override public void start() throws IOException { }
 	@Override public void stop() { }
@@ -25,12 +21,7 @@ public class MockPacketizer extends AbstractPacketizer {
 	public IStreamListener getListener () { return _streamListener; }
 	public InputStream getInputStream  () { return is;              }
 	public SessionType getSessionType  () { return _serviceType;    }
-	public SdlSession getSdlSession    () { return _session;    	}
 	public byte getSessionId           () { return _rpcSessionID;   }
-	public RPCRequest getRPCRequest    () { return _request;        }
-	@Deprecated
-	public byte getWiproVersion        () { if(_wiproVersion != null){return (byte)_wiproVersion.getMajor(); }else{return 5;}}
-	public Version getProtocolVersion  () { return _wiproVersion;   }
 
 	@Override public void pause() { }
 	@Override public void resume() { }
--- base/src/main/java/com/smartdevicelink/streaming/AbstractPacketizer.java
+++ base/src/main/java/com/smartdevicelink/streaming/AbstractPacketizer.java
@@ -49,35 +49,14 @@ abstract public class AbstractPacketizer {
 	protected InputStream is = null;
 	protected int bufferSize;
 	protected byte[] buffer;
-	protected boolean upts = false;
 	protected RPCRequest _request = null;
 	protected Version _wiproVersion = new Version("1.0.0");
-
-	//protected long ts = 0, intervalBetweenReports = 5000, delta = 0;
-	protected long intervalBetweenReports = 5000, delta = 0;
 
 	public AbstractPacketizer(IStreamListener streamListener, InputStream is, SessionType sType, byte rpcSessionID, SdlSession session) throws IOException, IllegalArgumentException {
 		this._streamListener = streamListener;
 		this.is = is;
 		_rpcSessionID = rpcSessionID;
 		_serviceType = sType;
-		this._session = session;
-		if (this._session != null) {
-			bufferSize = this._session.getMtu();
-			buffer = new byte[bufferSize];
-		}else{
-			throw new IllegalArgumentException("Session variable is null");
-		}
-	}
-
-	@Deprecated
-	public AbstractPacketizer(IStreamListener streamListener, InputStream is, RPCRequest request, SessionType sType, byte rpcSessionID, byte wiproVersion, SdlSession session) throws IOException, IllegalArgumentException {
-		this._streamListener = streamListener;
-		this.is = is;
-		_rpcSessionID = rpcSessionID;
-		_serviceType = sType;
-		_request = request;
-		_wiproVersion = new Version(wiproVersion+".0.0");
 		this._session = session;
 		if (this._session != null) {
 			bufferSize = this._session.getMtu();
