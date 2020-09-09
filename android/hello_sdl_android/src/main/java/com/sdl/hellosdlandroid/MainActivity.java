66487d28febdd61381b8860aee4240b858a772de
Bilal Alsharifi <bilal.alsharifi@gmail.com>
2020-09-09 11:59:00 -0400

    Fix warnings in hello sdl android

--- android/hello_sdl_android/src/main/java/com/sdl/hellosdlandroid/MainActivity.java
+++ android/hello_sdl_android/src/main/java/com/sdl/hellosdlandroid/MainActivity.java
@@ -7,8 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-	private static final String TAG = "MainActivity";
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
