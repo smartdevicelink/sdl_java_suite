6e85413b1d0b6b0bfead16f6b111f89776c3b4c2
Julian Kast <julian@livio.com>
2020-09-14 21:47:12 -0400

    Add java Docs to baseScreenManager

--- base/src/main/java/com/smartdevicelink/managers/screen/BaseScreenManager.java
+++ base/src/main/java/com/smartdevicelink/managers/screen/BaseScreenManager.java
@@ -378,6 +378,17 @@ abstract class BaseScreenManager extends BaseSubManager {
 		return this.textAndGraphicManager.getTitle();
 	}
 
+	/**
+	 * Change the current layout to a new layout and optionally update the layout's night and day color schemes. The values set for the text, graphics,
+	 * buttons and template title persist between layout changes. To update the text, graphics, buttons and template title at the same time as the template,
+	 * batch all the updates between `beginUpdates` and `endUpdates`. If the layout update fails while batching, then the updated text, graphics, buttons or template title will also not be updated.
+	 *
+	 * If you are connected on a < v6.0 connection and batching the update, the layout will be updated, then the text and graphics will be updated.
+	 * If you are connected on a >= v6.0 connection, the layout will be updated at the same time that the text and graphics are updated.
+	 *
+	 * @param templateConfiguration The new configuration of the template, including the layout and color scheme.
+	 * @param listener A listener that will be called when the layout change finished.
+	 */
 	public void changeLayout(TemplateConfiguration templateConfiguration, CompletionListener listener) {
 		textAndGraphicManager.changeLayout(templateConfiguration, listener);
 	}
