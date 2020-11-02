de84955899696f15754805430f5e0636da48ea6c
Bilal Alsharifi <bilal.alsharifi@gmail.com>
2020-11-02 12:10:23 -0500

    Update BuildConfig to use RC3_5.0.0

--- javaSE/javaSE/src/main/java/com/smartdevicelink/BuildConfig.java
+++ javaSE/javaSE/src/main/java/com/smartdevicelink/BuildConfig.java
@@ -32,5 +32,5 @@ package com.smartdevicelink;
 
 // THIS FILE IS AUTO GENERATED, DO NOT MODIFY!!
 public final class BuildConfig {
-    public static final String VERSION_NAME = "RC2_5.0.0";
+    public static final String VERSION_NAME = "RC3_5.0.0";
 }
