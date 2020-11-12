cf3bc8d1b920ec721bfd1a7db6075f27a0d05c39
Julian Kast <julian@livio.com>
2020-11-12 14:21:54 -0500

    Cleaned up AudioData and AlertAudioData, implemented creating TTSChunk in AudioData

--- base/src/main/java/com/smartdevicelink/managers/screen/alert/AlertAudioData.java
+++ base/src/main/java/com/smartdevicelink/managers/screen/alert/AlertAudioData.java
@@ -5,23 +5,37 @@ import androidx.annotation.NonNull;
 import com.smartdevicelink.managers.file.filetypes.SdlFile;
 import com.smartdevicelink.proxy.rpc.enums.SpeechCapabilities;
 
-public class AlertAudioData {
+public class AlertAudioData extends AudioData{
 
         private boolean playTone;
 
         // All vars have getters and setters
 
-        AlertAudioData(@NonNull SdlFile audioFile, @NonNull boolean playTone) {
-
-        }
-        AlertAudioData(@NonNull String spokenString, @NonNull boolean playTone) {
-
-        }
-        AlertAudioData(@NonNull String phoneticString, @NonNull SpeechCapabilities phoneticType, @NonNull boolean playTone) {
-
-        }
-        AlertAudioData(@NonNull boolean playTone) {
-
+        public AlertAudioData(@NonNull SdlFile audioFile, @NonNull boolean playTone) {
+                super(audioFile);
+                this.playTone = playTone;
         }
 
+        public AlertAudioData(@NonNull String spokenString, @NonNull boolean playTone) {
+                super(spokenString);
+                this.playTone = playTone;
+        }
+
+        public AlertAudioData(@NonNull String phoneticString, @NonNull SpeechCapabilities phoneticType, @NonNull boolean playTone) {
+                super(phoneticString, phoneticType);
+                this.playTone = playTone;
+        }
+
+        public AlertAudioData(@NonNull boolean playTone) {
+                super();
+                this.playTone = playTone;
+        }
+
+        public boolean isPlayTone() {
+                return playTone;
+        }
+
+        public void setPlayTone(boolean playTone) {
+                this.playTone = playTone;
+        }
 }
--- base/src/main/java/com/smartdevicelink/managers/screen/alert/AudioData.java
+++ base/src/main/java/com/smartdevicelink/managers/screen/alert/AudioData.java
@@ -5,23 +5,45 @@ import androidx.annotation.NonNull;
 import com.smartdevicelink.managers.file.filetypes.SdlFile;
 import com.smartdevicelink.proxy.rpc.TTSChunk;
 import com.smartdevicelink.proxy.rpc.enums.SpeechCapabilities;
+import com.smartdevicelink.util.DebugTool;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class AudioData {
 
+    private static final String TAG = "AudioData";
     private List<TTSChunk> prompt;
     private SdlFile audioFile;
 
     // All vars have getters as well but no setters
 
-    AudioData(@NonNull SdlFile audioFile) {
+    public AudioData() {
+    }
+
+    public AudioData(@NonNull SdlFile audioFile) {
         this.audioFile = audioFile;
     }
-    AudioData(@NonNull String spokenString) {
 
+    public AudioData(@NonNull String spokenString) {
+        prompt = Collections.singletonList(new TTSChunk().setText(spokenString));
     }
-    AudioData(@NonNull String phoneticString, @NonNull SpeechCapabilities phoneticType) {
 
+    public AudioData(@NonNull String phoneticString, @NonNull SpeechCapabilities phoneticType) {
+
+        if (!(phoneticType.equals(SpeechCapabilities.SAPI_PHONEMES) || phoneticType.equals(SpeechCapabilities.LHPLUS_PHONEMES)
+                || phoneticType.equals(SpeechCapabilities.TEXT) || phoneticType.equals(SpeechCapabilities.PRE_RECORDED))) {
+            return;
+        }
+        prompt = Collections.singletonList(new TTSChunk(phoneticString, phoneticType));
+    }
+
+    public SdlFile getAudioFile() {
+        return audioFile;
+    }
+
+    public List<TTSChunk> getPrompt() {
+        return prompt;
     }
 }
