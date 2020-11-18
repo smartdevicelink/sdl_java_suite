0d7b41745a85b8dc991e392594166f3c3f84175b
Henigan <rheniga1@MGC12Z921DLVCG.fbpld77.ford.com>
2020-11-18 10:17:37 -0500

    Add Changes from proposal for CustomPlaybackRate

--- base/src/main/java/com/smartdevicelink/proxy/rpc/SetMediaClockTimer.java
+++ base/src/main/java/com/smartdevicelink/proxy/rpc/SetMediaClockTimer.java
@@ -38,6 +38,7 @@ import com.smartdevicelink.protocol.enums.FunctionID;
 import com.smartdevicelink.proxy.RPCRequest;
 import com.smartdevicelink.proxy.rpc.enums.AudioStreamingIndicator;
 import com.smartdevicelink.proxy.rpc.enums.UpdateMode;
+import com.smartdevicelink.util.SdlDataTypeConverter;
 
 import java.util.Hashtable;
 
@@ -94,6 +95,16 @@ import java.util.Hashtable;
  * 			<td></td>
  * 			<td>SmartDeviceLink 5.0</td>
  * 		</tr>
+ * 	    <tr>
+ *          <td>countRate</td>
+ *          <td>Float</td>
+ *          <td>The value of this parameter is the amount that the media clock timer will advance per 1.0seconds of real time. Values less than 1.0 will therefore advance the timer slower thanreal-time, while values greater than 1.0 will advance the timer faster than real-time.e.g. If this parameter is set to `0.5`, the timer will advance one second per two secondsreal-time, or at 50% speed. If this parameter is set to `2.0`, the timer will advance twoseconds per one second real-time, or at 200% speed.</td>
+ *          <td>N</td>
+ *          <td>{"num_min_value": 0.1, "num_max_value": 100.0}</td>
+ *          <td>
+ *             @since SmartDeviceLink 7.1.0
+ *          </td>
+ *      </tr>
  *
  *  </table>
  *
@@ -117,6 +128,7 @@ public class SetMediaClockTimer extends RPCRequest {
     public static final String KEY_END_TIME = "endTime";
     public static final String KEY_UPDATE_MODE = "updateMode";
     public static final String KEY_AUDIO_STREAMING_INDICATOR = "audioStreamingIndicator";
+    public static final String KEY_COUNT_RATE = "countRate";
 
     /**
      * Constructs a new SetMediaClockTimer object
@@ -349,5 +361,39 @@ public class SetMediaClockTimer extends RPCRequest {
     public SetMediaClockTimer setAudioStreamingIndicator(AudioStreamingIndicator audioStreamingIndicator) {
         setParameters(KEY_AUDIO_STREAMING_INDICATOR, audioStreamingIndicator);
         return this;
+    }
+
+    /**
+     * Sets the countRate.
+     *
+     * @param countRate The value of this parameter is the amount that the media clock timer will advance per 1.0
+     * seconds of real time. Values less than 1.0 will therefore advance the timer slower than
+     * real-time, while values greater than 1.0 will advance the timer faster than real-time.
+     * e.g. If this parameter is set to `0.5`, the timer will advance one second per two seconds
+     * real-time, or at 50% speed. If this parameter is set to `2.0`, the timer will advance two
+     * seconds per one second real-time, or at 200% speed.
+     * {"num_min_value": 0.1, "num_max_value": 100.0}
+     * @since SmartDeviceLink 7.1.0
+     */
+    public SetMediaClockTimer setCountRate(Float countRate) {
+        setParameters(KEY_COUNT_RATE, countRate);
+        return this;
+    }
+
+    /**
+     * Gets the countRate.
+     *
+     * @return Float The value of this parameter is the amount that the media clock timer will advance per 1.0
+     * seconds of real time. Values less than 1.0 will therefore advance the timer slower than
+     * real-time, while values greater than 1.0 will advance the timer faster than real-time.
+     * e.g. If this parameter is set to `0.5`, the timer will advance one second per two seconds
+     * real-time, or at 50% speed. If this parameter is set to `2.0`, the timer will advance two
+     * seconds per one second real-time, or at 200% speed.
+     * {"num_min_value": 0.1, "num_max_value": 100.0}
+     * @since SmartDeviceLink 7.1.0
+     */
+    public Float getCountRate() {
+        Object object = getParameters(KEY_COUNT_RATE);
+        return SdlDataTypeConverter.objectToFloat(object);
     }
 }
