98bda1bcda511c913c532999f0d8e1fdcddac25c
Julian Kast <julian@livio.com>
2020-11-20 13:09:30 -0500

    Add AlertCompletionListener

--- base/src/main/java/com/smartdevicelink/managers/AlertCompletionListener.java
+++ base/src/main/java/com/smartdevicelink/managers/AlertCompletionListener.java
@@ -1,4 +1,11 @@
 package com.smartdevicelink.managers;
 
 public interface AlertCompletionListener {
+
+    /**
+     * Returns whether a specific operation was successful or not
+     *
+     * @param success - success or fail
+     */
+    void onComplete(boolean success, Integer tryAgainTime);
 }
