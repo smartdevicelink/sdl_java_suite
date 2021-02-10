7097577100cf814f546931e21452173388cd5b56
kboskin <kboskin>
2021-02-10 22:35:56 +0200

    [0293] - Align variables with Livio proposal

--- base/src/main/java/com/smartdevicelink/protocol/enums/ControlFrameTags.java
+++ base/src/main/java/com/smartdevicelink/protocol/enums/ControlFrameTags.java
@@ -86,12 +86,18 @@ public class ControlFrameTags {
              **/
             public static final String AUTH_TOKEN = "authToken";
             /**
-             * Vehicle related info
+             * Vehicle info to describe connected device
              */
-            public static final String VEHICLE_MAKE = "make";
-            public static final String VEHICLE_MODEL = "model";
-            public static final String VEHICLE_MODEL_YEAR = "modelYear";
-            public static final String VEHICLE_TRIM = "trim";
+            public static final String MAKE = "make";
+            public static final String MODEL = "model";
+            public static final String MODEL_YEAR = "modelYear";
+            public static final String TRIM = "trim";
+
+            /**
+             * System specifics for hardware and software versions of connected device
+             */
+            public static final String SYSTEM_SOFTWARE_VERSION = "systemSoftwareVersion";
+            public static final String SYSTEM_HARDWARE_VERSION = "systemHardwareVersion";
 
         }
 
