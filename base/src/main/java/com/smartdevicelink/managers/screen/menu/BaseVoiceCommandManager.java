d59080f2959cb4d37222ee2e99cf52f0b4aad1c9
Julian Kast <julian@livio.com>
2021-05-20 15:25:19 -0400

    Fix suggestions from code review

--- base/src/main/java/com/smartdevicelink/managers/screen/menu/BaseVoiceCommandManager.java
+++ base/src/main/java/com/smartdevicelink/managers/screen/menu/BaseVoiceCommandManager.java
@@ -205,6 +205,9 @@ abstract class BaseVoiceCommandManager extends BaseSubManager {
     List<VoiceCommand> removeEmptyVoiceCommands(List<VoiceCommand> voiceCommands) {
         List<VoiceCommand> validatedVoiceCommands = new ArrayList<>();
         for (VoiceCommand voiceCommand : voiceCommands) {
+            if (voiceCommand == null) {
+                continue;
+            }
             List<String> voiceCommandStrings = new ArrayList<>();
             for (String voiceCommandString : voiceCommand.getVoiceCommands()) {
                 if (voiceCommandString == null) {
@@ -273,7 +276,6 @@ abstract class BaseVoiceCommandManager extends BaseSubManager {
 
         for (VoiceCommand voiceCommand : voiceCommands) {
             if (voiceCommand == null) {
-                voiceCommands.remove(voiceCommand);
                 continue;
             }
             voiceCommandHashSet.addAll(voiceCommand.getVoiceCommands());
