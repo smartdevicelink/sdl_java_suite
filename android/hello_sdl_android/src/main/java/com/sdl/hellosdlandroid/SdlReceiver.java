c35b6f50727747d4fd270c40d3752ba99bdcb158
Yaroslav Lutsenko (GitHub) <yaroslav.lutsenko@dxc.com>
2021-07-16 12:23:01 +0300

    Remove changes in the 'HelloSdlApp'

--- android/hello_sdl_android/src/main/java/com/sdl/hellosdlandroid/SdlReceiver.java
+++ android/hello_sdl_android/src/main/java/com/sdl/hellosdlandroid/SdlReceiver.java
@@ -4,15 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 
-import com.smartdevicelink.proxy.rpc.VehicleType;
 import com.smartdevicelink.transport.SdlBroadcastReceiver;
 import com.smartdevicelink.transport.SdlRouterService;
-import com.smartdevicelink.transport.TransportConstants;
-import com.smartdevicelink.util.AndroidTools;
 import com.smartdevicelink.util.DebugTool;
-
-import java.util.HashMap;
-import java.util.List;
 
 public class SdlReceiver extends SdlBroadcastReceiver {
     private static final String TAG = "SdlBroadcastReceiver";
@@ -20,12 +14,6 @@ public class SdlReceiver extends SdlBroadcastReceiver {
     @Override
     public void onSdlEnabled(Context context, Intent intent) {
         DebugTool.logInfo(TAG, "SDL Enabled");
-
-        if (!IsVehicleTypeSupported(context, intent)) {
-            DebugTool.logError(TAG, "Vehicle type is not supported. Exiting");
-            return;
-        }
-
         intent.setClass(context, SdlService.class);
 
         // SdlService needs to be foregrounded in Android O and above
@@ -36,30 +24,6 @@ public class SdlReceiver extends SdlBroadcastReceiver {
         } else {
             context.startService(intent);
         }
-    }
-
-    private boolean IsVehicleTypeSupported(Context context, Intent intent) {
-        if (!intent.hasExtra(TransportConstants.VEHICLE_INFO)) {
-            DebugTool.logInfo(TAG, "Vehicle type is not supplied by the intent");
-            return true;
-        }
-
-        HashMap<String, Object> vehicleInfoStore = (HashMap<String, Object>) intent.getSerializableExtra(TransportConstants.VEHICLE_INFO);
-
-        VehicleType vehicleType;
-        if (vehicleInfoStore == null || vehicleInfoStore.isEmpty()) {
-            vehicleType = null;
-        } else {
-            vehicleType = new VehicleType(vehicleInfoStore);
-        }
-
-        if (vehicleType == null) {
-            DebugTool.logInfo(TAG, "Vehicle type is empty");
-            return true;
-        }
-
-        List<VehicleType> supportedList = AndroidTools.getVehicleTypesFromManifest(context, com.sdl.hellosdlandroid.SdlRouterService.class, R.string.sdl_oem_vehicle_type_filter_name);
-        return AndroidTools.isSupportableVehicleType(supportedList, vehicleType);
     }
 
     @Override
