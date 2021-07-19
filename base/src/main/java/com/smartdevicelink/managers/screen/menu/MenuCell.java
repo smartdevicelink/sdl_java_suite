580edf98690f65682f8b67263fecfbf3a5e9958e
Bilal Alsharifi <bilal.alsharifi@gmail.com>
2021-07-19 11:28:00 -0400

    Fix issue in MenuCell.hashCode()

--- base/src/main/java/com/smartdevicelink/managers/screen/menu/MenuCell.java
+++ base/src/main/java/com/smartdevicelink/managers/screen/menu/MenuCell.java
@@ -447,8 +447,8 @@ public class MenuCell implements Cloneable {
         int result = 1;
         result += ((getTitle() == null) ? 0 : Integer.rotateLeft(getTitle().hashCode(), 1));
         result += ((getIcon() == null) ? 0 : Integer.rotateLeft(getIcon().hashCode(), 2));
-        result += ((getVoiceCommands() == null) ? 0 : Integer.rotateLeft(getVoiceCommands().hashCode(), 3));
-        result += ((getSubCells() == null) ? 0 : Integer.rotateLeft(getSubCells().hashCode(), 4));
+        result += ((getVoiceCommands() == null || getVoiceCommands().isEmpty()) ? 0 : Integer.rotateLeft(getVoiceCommands().hashCode(), 3));
+        result += ((getSubCells() == null || getSubCells().isEmpty()) ? 0 : Integer.rotateLeft(getSubCells().hashCode(), 4));
         result += ((getSecondaryText() == null) ? 0 : Integer.rotateLeft(getSecondaryText().hashCode(), 1));
         result += ((getTertiaryText() == null) ? 0 : Integer.rotateLeft(getTertiaryText().hashCode(), 1));
         result += ((getSecondaryArtwork() == null) ? 0 : Integer.rotateLeft(getSecondaryArtwork().hashCode(), 2));
