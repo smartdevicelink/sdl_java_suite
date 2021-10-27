327d8d674c91368ee40931e9309eab0f15c864a8
RHeniz <heniganr1@gmail.com>
2021-10-27 13:41:45 -0400

    Only set immutable flag if android 12 or newer

--- android/sdl_android/src/main/java/com/smartdevicelink/transport/SdlRouterService.java
+++ android/sdl_android/src/main/java/com/smartdevicelink/transport/SdlRouterService.java
@@ -1528,7 +1528,8 @@ public class SdlRouterService extends Service {
 
         // Create an intent that will be fired when the user clicks the notification.
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(SDL_NOTIFICATION_FAQS_PAGE));
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+        int flag = android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.S ? PendingIntent.FLAG_IMMUTABLE : 0;
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, flag);
         builder.setContentIntent(pendingIntent);
 
         if (chronometerLength > (FOREGROUND_TIMEOUT / 1000) && android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
