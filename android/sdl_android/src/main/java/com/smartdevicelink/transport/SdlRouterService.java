6fd76ae6e4bd93402046b198f19998c85b1f0b9a
RHenigan <heniganr1@gmail.com>
2022-02-14 15:15:38 -0500

    Router Service Version increase

--- android/sdl_android/src/main/java/com/smartdevicelink/transport/SdlRouterService.java
+++ android/sdl_android/src/main/java/com/smartdevicelink/transport/SdlRouterService.java
@@ -143,7 +143,7 @@ public class SdlRouterService extends Service {
     /**
      * <b> NOTE: DO NOT MODIFY THIS UNLESS YOU KNOW WHAT YOU'RE DOING.</b>
      */
-    protected static final int ROUTER_SERVICE_VERSION_NUMBER = 15;
+    protected static final int ROUTER_SERVICE_VERSION_NUMBER = 16;
 
     private static final String ROUTER_SERVICE_PROCESS = "com.smartdevicelink.router";
 
