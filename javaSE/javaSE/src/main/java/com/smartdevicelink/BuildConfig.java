c53583aced67b3db184b92a0159bc060ec0ce4e9
“JKAST” <julian.kast@live.com>
2022-06-21 12:44:20 -0400

    Update Version name and version code for 5.5.0_RC

--- javaSE/javaSE/src/main/java/com/smartdevicelink/BuildConfig.java
+++ javaSE/javaSE/src/main/java/com/smartdevicelink/BuildConfig.java
@@ -32,5 +32,5 @@ package com.smartdevicelink;
 
 // THIS FILE IS AUTO GENERATED, DO NOT MODIFY!!
 public final class BuildConfig {
-    public static final String VERSION_NAME = "5.4.0";
+    public static final String VERSION_NAME = "5.5.0_RC";
 }
