8479e868da25e8f3c61adb01d10033aab73f18d5
“JKAST” <julian.kast@live.com>
2022-09-22 14:21:22 -0400

    Remove unused import

--- base/src/main/java/com/smartdevicelink/managers/screen/PresentAlertOperation.java
+++ base/src/main/java/com/smartdevicelink/managers/screen/PresentAlertOperation.java
@@ -57,7 +57,6 @@ import com.smartdevicelink.util.DebugTool;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
