6af968d1afcdff755965c000a93fafe2848c6b4a
“JKAST” <julian.kast@live.com>
2024-02-09 09:18:22 -0500

    test to find version code

--- android/sdl_android/src/androidTest/java/com/smartdevicelink/test/transport/MultiplexBluetoothTransportTest.java
+++ android/sdl_android/src/androidTest/java/com/smartdevicelink/test/transport/MultiplexBluetoothTransportTest.java
@@ -53,11 +53,41 @@ public class MultiplexBluetoothTransportTest extends TestCase {
         assertEquals(bluetooth.getState(), MultiplexBluetoothTransport.STATE_NONE);
 
         bluetooth.start();
-        if (DeviceUtil.isEmulator() && Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+/*        if (DeviceUtil.isEmulator() && Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
             assertEquals(bluetooth.getState(), MultiplexBluetoothTransport.STATE_NONE);
         } else {
             assertEquals(bluetooth.getState(), MultiplexBluetoothTransport.STATE_LISTEN);
-        }
+        }*/
+         if(Build.VERSION.SDK_INT == Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+             assertEquals(34, bluetooth.getState());
+
+        } else if(Build.VERSION.SDK_INT == Build.VERSION_CODES.TIRAMISU) {
+             assertEquals(33, bluetooth.getState());
+
+         } else if(Build.VERSION.SDK_INT == Build.VERSION_CODES.S_V2) {
+             assertEquals(32, bluetooth.getState());
+
+         }  else if(Build.VERSION.SDK_INT == Build.VERSION_CODES.S) {
+             assertEquals(31, bluetooth.getState());
+
+         }else if(Build.VERSION.SDK_INT == Build.VERSION_CODES.R) {
+             assertEquals(30, bluetooth.getState());
+
+        } else if(Build.VERSION.SDK_INT == Build.VERSION_CODES.Q) {
+             assertEquals(29, bluetooth.getState());
+
+        } else if(Build.VERSION.SDK_INT == Build.VERSION_CODES.P) {
+             assertEquals(28, bluetooth.getState());
+        } else if(Build.VERSION.SDK_INT == Build.VERSION_CODES.O) {
+             assertEquals(26, bluetooth.getState());
+
+        }  else if(Build.VERSION.SDK_INT == Build.VERSION_CODES.JELLY_BEAN) {
+             assertEquals(16, bluetooth.getState());
+
+        } else {
+             assertEquals(Build.VERSION.SDK_INT, bluetooth.getState());
+
+         }
 
         bluetooth.stop();
         assertEquals(bluetooth.getState(), MultiplexBluetoothTransport.STATE_NONE);
