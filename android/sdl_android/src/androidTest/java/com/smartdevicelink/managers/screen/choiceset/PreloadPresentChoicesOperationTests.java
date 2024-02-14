1c6329175540a29ebad3ba9ecba7c5729970d383
“JKAST” <julian.kast@live.com>
2024-02-14 15:09:04 -0500

    update manager layer test sleep calls to 1 sec

--- android/sdl_android/src/androidTest/java/com/smartdevicelink/managers/screen/choiceset/PreloadPresentChoicesOperationTests.java
+++ android/sdl_android/src/androidTest/java/com/smartdevicelink/managers/screen/choiceset/PreloadPresentChoicesOperationTests.java
@@ -246,7 +246,7 @@ public class PreloadPresentChoicesOperationTests {
 
     private void sleep() {
         try {
-            Thread.sleep(100);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
